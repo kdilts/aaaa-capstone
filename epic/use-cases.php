11f5ea929dd67e2817cb00e19fa08f17dcfda5cf
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 15:27:58 -0600

    initial

---
+++ epic/use-cases.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>
+
+		</title>
+	</head>
+	<body>
+		<h3>
+			Use Cases:
+
+			I'd want to quickly look up, for example, all students who have applied for the July bootcamp and have paid and been emailed their pre-work.
+
+			Or I'd like to know who I sent an Access Funds application to.
+
+			or who have I sent a registration form to.
+
+			Karen B could want to see who returned their parking passes.
+
+			Karen G could report the average salary and percentage of students who found work.
+
+			Karen G could identify which students have good PHP skills for a potential employer.
+
+			Bridge staff track students pre-work progress and text book distribution.
+		</h3>
+	</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/27/2016
+ * Time: 3:27 PM
+ */
