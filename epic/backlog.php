c194f191e18a5deb2f3a9713d50ac1143f4127bb
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 15:24:21 -0600

    initial

---
+++ epic/backlog.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>
+
+		</title>
+	</head>
+	<body>
+		<h3>
+			create mySQL tables / database structure
+			entities / PHP classes
+
+			front end design / wireframe
+			view for applicants
+			view for andrea / administrators
+
+			make ERD diagram
+
+			finish epic
+		</h3>
+	</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/27/2016
+ * Time: 3:22 PM
+ */
