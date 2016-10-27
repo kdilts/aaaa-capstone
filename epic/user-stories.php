cbaf76217a02c7d9a1f42ae540dbfbca2705fdb5
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 15:28:54 -0600

    initial

---
+++ epic/user-stories.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>
+
+		</title>
+	</head>
+	<body>
+		<h3>
+			Applicants are using our site so that they may easily submit applications to the program.
+
+			Andrea is using our site so that she may easily track inquiries, applications, and payment information on each potential student.
+
+			The bridge staff is using our site so that they may keep track of student's progress on their pre work, as well as textbook distribution.
+
+			Administrative staff is using our site so that they can create notes on any and all interactions with students as well as collect demographic data and statistics.
+		</h3>
+	</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/27/2016
+ * Time: 3:28 PM
+ */
