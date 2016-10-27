fb7687f81eae7ca1f5156a274af76ad5a0fee21b
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 15:25:19 -0600

    initial

---
+++ epic/executive-summary.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>
+
+		</title>
+	</head>
+	<body>
+		<h3>
+			Currently Bootcamp inquiries and applications are emailed to CNM Stemulus administration who have to manually enter the information from each into the database. Our project is an automated system for keeping track of CNM Stemulus applicants, with the ability for administrators to make notes, and modify data on the fly. It will work as an addition to CNM's existing database and systems.
+		</h3>
+	</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/27/2016
+ * Time: 3:24 PM
+ */
