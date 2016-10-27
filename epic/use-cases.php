4fdfad1ec2a6acabd80e35633114676feee9eb78
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 15:52:32 -0600

    initial

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -10,15 +10,8 @@
 
 			<li>I'd want to quickly look up, for example, all students who have applied for the July bootcamp and have paid and been emailed their pre-work.</li>
 
-			<li>I'd like to know who I sent an Access Funds application to.\
-
-			or who have I sent a registration form to.
 
 			<li>Karen B could want to see who returned their parking passes.</li>
-
-			<li>Karen G could report the average salary and percentage of students who found work.</li>
-
-			<li>Karen G could identify which students have good PHP skills for a potential employer.</li>
 
 			<li>Bridge staff track students pre-work progress and text book distribution.</li>
 		</ul>
