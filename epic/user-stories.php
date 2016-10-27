d5b8ed3e7e13d0755df5844754151cd44c49631b
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 16:08:01 -0600

    initial

--- epic/user-stories.php
+++ epic/user-stories.php
@@ -4,16 +4,17 @@
 
 		</title>
 	</head>
-	<body>
-		<h3>
-			Applicants are using our site so that they may easily submit applications to the program.
+	<body><ul>
+			Use Cases:
 
-			Andrea is using our site so that she may easily track inquiries, applications, and payment information on each potential student.
+			<li>I'd want to quickly look up, for example, all students who have applied for the July bootcamp and have paid and been emailed their pre-work.</li>
 
-			The bridge staff is using our site so that they may keep track of student's progress on their pre work, as well as textbook distribution.
 
-			Administrative staff is using our site so that they can create notes on any and all interactions with students as well as collect demographic data and statistics.
-		</h3>
+			<li>Karen B could want to see who returned their parking passes.</li>
+
+			<li>Bridge staff track students pre-work progress and text book distribution.</li>
+		</ul>
+
 	</body>
 </html>
 <?php
