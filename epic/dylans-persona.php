cfd3fb36ee26ba5b0074d7236b3822e4fc699bfd
Trevor Pelton <tnpflyer20@msn.com>
2016-10-27 15:33:35 -0600

    initial

---
+++ epic/dylans-persona.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>
+
+		</title>
+	</head>
+	<body>
+		<h3>
+			<img src="https://i.imgur.com/MQHYB.jpg">
+		</h3>
+	</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/27/2016
+ * Time: 3:32 PM
+ */
