0c7149f77a570fa55b367a90062d52edd931ccf5
Kevin Dilts <kdilts13@gmail.com>
2016-10-27 16:13:46 -0600

    updated persona

--- epic/persona.php
+++ epic/persona.php
@@ -5,23 +5,33 @@
 		</title>
 	</head>
 	<body>
-		<h3>
-			Persona
-			Name: Andrea
 
-			Profession: Logistic ninja at STEMUlus Academy> Full time Inquiries, Registration and Payments Administrator
-			Techology: Surface Pro, Windows 10 with MS Windows Specifically MS Access as well as CNM Webmail/Outlook. Android Phone.
+		<h1>Persona</h1>
+		<ul>
+			<li>
+				Name: Andrea
+			</li>
+			<li>
+				Profession: Logistic ninja at STEMUlus Academy> Full time Inquiries, Registration and Payments Administrator
+			</li>
+			<li>
+				Techology: Surface Pro, Windows 10 with MS Windows Specifically MS Access as well as CNM Webmail/Outlook. Android Phone.
+			</li>
 
-			Attitudes and Behaviors: Andrea makes sure all the concerns of the applicants are addressed in a profesional and friendly fashion.
-			She is very overloaded in administrative tasks. She is the primary contact for all the inquiries for the cohorts
-			and in charge of registration and payments for applicants. She does not want to enter each interaction into the database by hand.
+			<li>
+				Attitudes and Behaviors: Andrea makes sure all the concerns of the applicants are addressed in a profesional and friendly fashion.
+				She is very overloaded in administrative tasks. She is the primary contact for all the inquiries for the cohorts
+				and in charge of registration and payments for applicants. She does not want to enter each interaction into the database by hand.
+			</li>
 
-			Frustrations and Needs: She is very frustrated about the complete manual process across the board on the record keeping for applications, payments and personal records.
-			She needs a system where she may keep notes on each individual interactions with the inquirers and applicants as they happen.
-
-			Goals: Consistent and easy access of records, reduction of manual process and a process to upgrade from inquiries to applications!
-
-		</h3>
+			<li>
+				Frustrations and Needs: She is very frustrated about the complete manual process across the board on the record keeping for applications, payments and personal records.
+				She needs a system where she may keep notes on each individual interactions with the inquirers and applicants as they happen.
+			</li>
+			<li>
+				Goals: Consistent and easy access of records, reduction of manual process and a process to upgrade from inquiries to applications!
+			</li>
+		</ul>
 	</body>
 </html>
 <?php
