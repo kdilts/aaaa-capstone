09f242eb40f6082c768907604732cefb85e5cc13
Kevin Dilts <kdilts13@gmail.com>
2016-10-27 16:18:41 -0600

    added to user stories

--- epic/user-stories.php
+++ epic/user-stories.php
@@ -4,15 +4,32 @@
 
 		</title>
 	</head>
-	<body><ul>
-			User Stories:
+	<body>
+		<h1>User Stories:</h1>
+		<ul>
+			<li>
+				I'd want to quickly look up, for example, all students who have applied for the July bootcamp and have paid and been emailed their pre-work.
+			</li>
 
-			<li>I'd want to quickly look up, for example, all students who have applied for the July bootcamp and have paid and been emailed their pre-work.</li>
+			<li>
+				Karen B could want to see who returned their parking passes.
+			</li>
 
+			<li>
+				Bridge staff track students pre-work progress and text book distribution.
+			</li>
 
-			<li>Karen B could want to see who returned their parking passes.</li>
+			<li>
+				Dylan wants to record whenever someone creates a mySQL instance.
+			</li>
 
-			<li>Bridge staff track students pre-work progress and text book distribution.</li>
+			<li>
+				Dylan wants to create notes on new applicants
+			</li>
+
+			<li>
+				Dylan wants to track who has gone through the invite system / who is lagging.
+			</li>
 		</ul>
 
 	</body>
