dd9ab5048e7282c7aaa88729477065d87a78b51f
Kevin Dilts <kdilts13@gmail.com>
2016-10-27 16:11:57 -0600

    updated exec summary

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -5,9 +5,9 @@
 		</title>
 	</head>
 	<body>
-		<h3>
+		<p>
 			Currently Bootcamp inquiries and applications are emailed to CNM Stemulus administration who have to manually enter the information from each into the database. Our project is an automated system for keeping track of CNM Stemulus applicants, with the ability for administrators to make notes, and modify data on the fly. It will work as an addition to CNM's existing database and systems.
-		</h3>
+		</>
 	</body>
 </html>
 <?php
