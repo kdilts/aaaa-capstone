0f6329619cf0996d3dc4ac2f5084c3b0ae4dce7a
Maria Rosado <mrosado2@cnm.edu>
2016-10-28 10:36:27 -0600

    updated Andreas use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -6,31 +6,32 @@
 	</head>
 	<body>
 		<h1>Use Cases</h1>
-		<ul>
-			<li>Andrea</li>
+
+			<h1>Andrea</h1>
+			<ul>
 			<li>Andrea works as the logistics admimistrator for SETMUlus Center.</li>
 			<li>Andres is the point of contact for all the inquiries on regards to the cohorts offered by the center.</li>
 			<li>She keeps records of those individuals phone calls, texts and emails.</li>
-				<li>If they express a desire to start the process then she upgrades them to applicants.</li>
-				<li>Andrea also handles all the applicants</li>
-				<li>When Andrea receives a request for an application she emails the documents, once the application is
-					submetted she will send a message that lays out the process to submit it the payment process and the
-					requirement on regards to the mandatory prework.</li>
-					<li>with every concern  each applicant, and record
-				keeping of applicants to the multiples cohorts offered by the center. She
-			wants to have an automated system to keep records and where she may also have a one click access to the
-			information for updates add comments and report to the Bridge about students that completed payments and how many
+			<li>If they express a desire to start the process then she upgrades them to applicants.</li>
+			<li>Andrea also handles all the applicants</li>
+			<li>When Andrea receives a request for an application she emails the documents, once the application is
+				submetted she will send a message that lays out the process to submit it the payment process and the
+				requirement on regards to the mandatory prework.</li>
+			<li>Andrea manually registers the applicants to the multiples cohorts offered by the center.</li>
+			<li>Andrea also keeps all the records of the applicants in different applications depending on what the record
+				is about. Like emails phone calls and texts. Payment status. Set up to receive prework, books and schedules.</li>
+			<li>Andrea wants to have an automated system to keep records and where she may also have a one click access to the
+			information for updates add comments and reports to the Bridge about students that completed payments and how many
 			students there are for each cohort</li>
-		<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
-			places that she has to access to answer questions from the staff, CNM and students</li>
+			<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
+				places that she has to access to answer questions from the staff, CNM and students</li>
 			<li>All that takes a lot of her time and effort that she does not even have time left after work to visit the
-			Bosque</li>
-		<li>She would like that each one of interested parties would have access to the information they need withour having
-			to recurr to her for every single question</li>
-		<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
-			knowledge of the opportunities the center offers.
-			t</li>
-			</ul>
+				Bosque</li>
+			<li>She would like that each one of interested parties would have access to the information they need withour having
+				to recurr to her for every single question</li>
+			<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
+				knowledge of the opportunities the center offers.</li>
+		</ul>
 		<ul>
 			Karen:
 			<li>One of Karen's responsibilities is keeping track of parking permits. Who has one, who needs one, who hasn't returned theirs, etc. She needs a function in the site and database that keeps track of this information. If possible the system should have Alerts set up to notify and remind. </li>
