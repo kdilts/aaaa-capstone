fe8be6366400c84366ea694f4b16b8e51bf20e28
Maria Rosado <mrosado2@cnm.edu>
2016-10-28 09:16:33 -0600

    updated Andreas use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -9,10 +9,10 @@
 		<ul>
 			<li>Andrea</li>
 		<li>Andrea works as the logistics admimistrator for SETMUlus Center. She is in charge of keeping track of all the
-			inquiries, applications, of applicants to the multiples cohorts offered by the center. She wants to have an
-			automated system to keep records and where she may also have a one click access to the information for updates
-			add comments and report to the Bridge about students that completed payments and how many students there are
-			for each cohort</li>
+			inquiries, applications, and record keeping of applicants to the multiples cohorts offered by the center. She
+			wants to have an automated system to keep records and where she may also have a one click access to the
+			information for updates add comments and report to the Bridge about students that completed payments and how many
+			students there are for each cohort</li>
 		<li>She is overwhelmed by the manual procedure to keep track of every phone call and the different places that she
 			has to access to answer questions from the staff, CNM and students</li>
 		<li>She would like that each one of interested parties would have access to the information they need withour having
