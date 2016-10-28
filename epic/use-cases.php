e0c503c121daf2c8e8ff19affb5242cb12a0fcbd
jeremiahwood <jzwood47@cnm.edu>
2016-10-28 10:09:17 -0600

    Merge branch 'master' of /Users/jeremiahwood/Desktop/Book Camp/Git/aaaa-capstone with conflicts.

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -7,26 +7,27 @@
 	<body>
 		<h1>Use Cases</h1>
 		<ul>
-			Andrea
-		<li>Andrea works as the logistics admimistrator for SETMUlus Center. She is in charge of keeping track of all the
-			inquiries, applications, and record keeping of applicants to the multiples cohorts offered by the center. She
-			wants to have an automated system to keep records and where she may also have a one click access to the
-			information for updates add comments and report to the Bridge about students that completed payments and how many
-			students there are for each cohort</li>
-		<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
-			places that she has to access to answer questions from the staff, CNM and students</li>
+			<li>Andrea</li>
+			<li>Andrea works as the logistics admimistrator for SETMUlus Center. She is in charge of keeping track of all the
+				inquiries, applications, and record keeping of applicants to the multiples cohorts offered by the center. She
+				wants to have an automated system to keep records and where she may also have a one click access to the
+				information for updates add comments and report to the Bridge about students that completed payments and how many
+				students there are for each cohort</li>
+			<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
+				places that she has to access to answer questions from the staff, CNM and students</li>
 			<li>All that takes a lot of her time and effort that she does not even have time left after work to visit the
-			Bosque</li>
-		<li>She would like that each one of interested parties would have access to the information they need withour having
-			to recurr to her for every single question</li>
-		<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
-			knowledge of the opportunities the center offers.
-			t</li>
-			</ul>
-		<ul>
-			Karen:
-			<li>One of Karen's responsibilities is keeping track of parking permits. Who has one, who needs one, who hasn't returned theirs, etc. She needs a function in the site and database that keeps track of this information. If possible the system should have Alerts set up to notify and remind. </li>
+				Bosque</li>
+			<li>She would like that each one of interested parties would have access to the information they need withour having
+				to recurr to her for every single question</li>
+			<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
+				knowledge of the opportunities the center offers.
+				t</li>
 		</ul>
+		<h2> Use Case Dylan "Captan"</h2>
+		<p> Dylan AKA the “Captan”   never has much down time and that is by personal choice for the most part.  Dylan has just received my application for the Stimulus Program via from Andrea. With this new software he can easily make notes and adjustments to the applicants while keeping track of there progress in the pre work stage. </p>
+		<p> After submitting my application I go down to CNM to make a payment to begin the process of bettering my life. After making my payment in cash from selling a kidney.  I quickly receive  an email as a notification.</p>
+		<p> Dylan logs back into the AAAA program to see if I and or any other applicants  have started there pre-work.  He sees that I have made my payment and started to  1. created my CNM and Deep Dive student accounts 2. set up the toolset and need to get my book so I can self-study.</p>
+		<p> Since the installment of the new program, Dylan now has spare time to dress caption Arlo up as a Klingon and reenact  scenes from the bridge of Star Trek V.</p>
 	</body>
 </html>
 <?php
