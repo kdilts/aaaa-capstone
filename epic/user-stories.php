9c474ee1ae7097abf547757a713775acf7a22400
Kevin Dilts <kdilts13@gmail.com>
2016-10-28 08:33:27 -0600

    re organized user stories

--- epic/user-stories.php
+++ epic/user-stories.php
@@ -1,67 +1,80 @@
 <html>
 	<head>
 		<title>
-
+			User Stories
 		</title>
+		<style>
+			li {
+				margin: 10px auto;
+			}
+		</style>
 	</head>
 	<body>
 		<h1>User Stories:</h1>
 		<ul>
 			<li>
-				I'd want to quickly look up, for example, all students who have applied for the July Bootcamp and have paid and been emailed their pre-work.
+				Andrea
+				<ul>
+					<li>
+						I'd want to quickly look up, for example, all students who have applied for the July Bootcamp and have paid and been emailed their pre-work.
+					</li>
+					<li>
+						As a user i want to be able to log in and see all current applicants statuses, who has completed payment who hasn't, any flags comments etc.
+					</li>
+					<li>
+						Andrea is using our site so that she may easily track inquiries from each potential student.
+					</li>
+					<li>
+						Andrea is using our site so that she may easily track applications from each potential student.
+					</li>
+					<li>
+						Andrea is using our site so that she may easily track payment from each potential student.
+					</li>
+				</ul>
 			</li>
-
+			<li>
+				Dylan
+				<ul>
+					<li>
+						Dylan wants to record whenever someone creates a mySQL instance.
+					</li>
+					<li>
+						Dylan wants to create notes on new applicants.
+					</li>
+					<li>
+						Dylan wants to track who has gone through the invite system / who is lagging.
+					</li>
+				</ul>
+			</li>
+			<li>
+				Bridge Staff
+				<ul>
+					<li>
+						Bridge staff track students pre-work progress and text book distribution.
+					</li>
+					<li>
+						The bridge staff is using our site so that they may keep track of student's progress on their pre work.
+					</li>
+					<li>
+						The bridge staff is using our site so that they may keep track of textbook distribution.
+					</li>
+				</ul>
+			</li>
+			<li>
+				Administrative staff
+				<ul>
+					<li>
+						Administrative staff is using our site so that they can create notes on any and all interactions with students.
+					</li>
+					<li>
+						Administrative staff is using our site so that they can collect demographic data and statistics.
+					</li>
+				</ul>
+			</li>
 			<li>
 				Karen B could want to see who returned their parking passes.
 			</li>
 
-			<li>
-				Bridge staff track students pre-work progress and text book distribution.
-			</li>
-
-			<li>
-				Dylan wants to record whenever someone creates a mySQL instance.
-			</li>
-
-			<li>
-				Dylan wants to create notes on new applicants.
-			</li>
-
-			<li>
-				Dylan wants to track who has gone through the invite system / who is lagging.
-			</li>
-
-			<li>
-				Andrea is using our site so that she may easily track inquiries from each potential student.
-			</li>
-
-			<li>
-				Andrea is using our site so that she may easily track applications from each potential student.
-			</li>
-
-			<li>
-				Andrea is using our site so that she may easily track payment from each potential student.
-			</li>
-
-			<li>
-				The bridge staff is using our site so that they may keep track of student's progress on their pre work.
-			</li>
-
-			<li>
-				The bridge staff is using our site so that they may keep track of textbook distribution.
-			</li>
-
-			<li>
-				Administrative staff is using our site so that they can create notes on any and all interactions with students.
-			</li>
-
-			<li>
-				Administrative staff is using our site so that they can collect demographic data and statistics.
-			</li>
-
-			<li>
-				As a user i want to be able to log in and see all current applicants statuses, who has completed payment who hasn't, any flags comments etc.
-			</li>
 		</ul>
 
 	</body>
