1e60cb0455efcaa2b129bdef874302fe0186ef77
Maria Rosado <mrosado2@cnm.edu>
2016-10-28 10:14:19 -0600

    updated Andreas use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -8,30 +8,29 @@
 		<h1>Use Cases</h1>
 		<ul>
 			<li>Andrea</li>
-			<li>Andrea works as the logistics admimistrator for SETMUlus Center. She is in charge of keeping track of all the
-				inquiries, applications, and record keeping of applicants to the multiples cohorts offered by the center. She
-				wants to have an automated system to keep records and where she may also have a one click access to the
-				information for updates add comments and report to the Bridge about students that completed payments and how many
-				students there are for each cohort</li>
-			<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
-				places that she has to access to answer questions from the staff, CNM and students</li>
+			<li>Andrea works as the logistics admimistrator for SETMUlus Center.</li>
+			<li>Andres is the point of contact for all the inquiries on regards to the cohorts offered by the center.</li>
+			<li>She keeps records of those individuals phone calls, texts and emails.</li>
+				<li>If they express a desire to start the process then she upgrades them to applicants.</li>
+				<li>Andrea also handles all the applicants</li>
+				<li>When Andrea receives a request for an application she emails the documents, once the application is
+					submetted she will send a message that lays out the process to submit it the payment process and the
+					requirement on regards to the mandatory prework.</li>
+					<li>with every concern  each applicant, and record
+				keeping of applicants to the multiples cohorts offered by the center. She
+			wants to have an automated system to keep records and where she may also have a one click access to the
+			information for updates add comments and report to the Bridge about students that completed payments and how many
+			students there are for each cohort</li>
+		<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
+			places that she has to access to answer questions from the staff, CNM and students</li>
 			<li>All that takes a lot of her time and effort that she does not even have time left after work to visit the
-				Bosque</li>
-			<li>She would like that each one of interested parties would have access to the information they need withour having
-				to recurr to her for every single question</li>
-			<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
-				knowledge of the opportunities the center offers.
-				t</li>
-		</ul>
-		<ul>
-			Karen:
-			<li>One of Karen's responsibilities is keeping track of parking permits. Who has one, who needs one, who hasn't returned theirs, etc. She needs a function in the site and database that keeps track of this information. If possible the system should have Alerts set up to notify and remind. </li>
-		</ul>
-		<h2> Use Case Dylan "Captan"</h2>
-		<p> Dylan AKA the “Captan”   never has much down time and that is by personal choice for the most part.  Dylan has just received my application for the Stimulus Program via from Andrea. With this new software he can easily make notes and adjustments to the applicants while keeping track of there progress in the pre work stage. </p>
-		<p> After submitting my application I go down to CNM to make a payment to begin the process of bettering my life. After making my payment in cash from selling a kidney.  I quickly receive  an email as a notification.</p>
-		<p> Dylan logs back into the AAAA program to see if I and or any other applicants  have started there pre-work.  He sees that I have made my payment and started to  1. created my CNM and Deep Dive student accounts 2. set up the toolset and need to get my book so I can self-study.</p>
-		<p> Since the installment of the new program, Dylan now has spare time to dress caption Arlo up as a Klingon and reenact  scenes from the bridge of Star Trek V.</p>
+			Bosque</li>
+		<li>She would like that each one of interested parties would have access to the information they need withour having
+			to recurr to her for every single question</li>
+		<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
+			knowledge of the opportunities the center offers.
+			t</li>
+			</ul>
 	</body>
 </html>
 <?php
