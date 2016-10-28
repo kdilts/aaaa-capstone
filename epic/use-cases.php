d4b9d2ec62db500a7ae8c725a0e6d4cfc7aadea0
Maria Rosado <mrosado2@cnm.edu>
2016-10-28 09:19:35 -0600

    updated Andreas use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -13,8 +13,10 @@
 			wants to have an automated system to keep records and where she may also have a one click access to the
 			information for updates add comments and report to the Bridge about students that completed payments and how many
 			students there are for each cohort</li>
-		<li>She is overwhelmed by the manual procedure to keep track of every phone call and the different places that she
-			has to access to answer questions from the staff, CNM and students</li>
+		<li>She is overwhelmed by the manual procedure to keep track of every phone call and record like and the different
+			places that she has to access to answer questions from the staff, CNM and students</li>
+			<li>All that takes a lot of her time and effort that she does not even have time left after work to visit the
+			Bosque</li>
 		<li>She would like that each one of interested parties would have access to the information they need withour having
 			to recurr to her for every single question</li>
 		<li>She would really like to have more of her time dedicated to promote the program, and the ways more people have
