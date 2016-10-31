95450475f63f5305dbc48affd92d199527a8b203
Kevin Dilts <kdilts13@gmail.com>
2016-10-31 10:22:19 -0600

    set doctype and meta tags

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -1,8 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-		<title>
-
-		</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<title>Use Cases</title>
 	</head>
 	<body>
 		<h1>Use Cases</h1>
