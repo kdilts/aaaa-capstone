e42f7beb9cf557b90e8540afc585032b08027432
Kevin Dilts <kdilts13@gmail.com>
2016-10-31 15:06:13 -0600

    continuing to design tables

--- public_html/sql/database.sql
+++ public_html/sql/database.sql
@@ -26,9 +26,35 @@ CREATE TABLE prospect(
 	FOREIGN KEY(prospectCohortId) REFERENCES cohort(cohortId)
 );
 
-CREATE TABLE student();
+CREATE TABLE student(
+	studentId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	studentFirstName VARCHAR(40) NOT NULL,
+	studentLastName VARCHAR(40) NOT NULL,
+	studentEmail VARCHAR(100) NOT NULL,
+	studentPhoneNumber VARCHAR(30) NOT NULL,
+	studentCohortId INT UNSIGNED NOT NULL,
+	studentDateOfBirth DATE NOT NULL,
+	studentAddress VARCHAR(200) NOT NULL,
+	INDEX (studentId),
+	PRIMARY KEY(studentId),
+	FOREIGN KEY(studentCohortId) REFERENCES cohort(cohortId)
+);
 
-CREATE TABLE application();
+CREATE TABLE application(
+	applicationId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	applicationFirstName VARCHAR(40) NOT NULL,
+	applicationLastName VARCHAR(40) NOT NULL,
+	applicationEmail VARCHAR(100) NOT NULL,
+	applicationPhoneNumber VARCHAR(30) NOT NULL,
+	applicationSource TEXT NOT NULL,
+	applicationCohortId TEXT NOT NULL,
+	applicationAboutYou TEXT NOT NULL,
+	applicationHopeToAccomplish TEXT NOT NULL,
+	applicationExperience TEXT NOT NULL,
+	INDEX (applicationId),
+	PRIMARY KEY(applicationId)
+	FOREIGN KEY(applicationCohortId) REFERENCES cohort(cohortId)
+);
 
 CREATE TABLE note();
 
