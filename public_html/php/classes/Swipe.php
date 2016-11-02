5d9987bd16fa855d158cda5250c5090d4527170d
Kevin Dilts <kdilts13@gmail.com>
2016-11-02 15:21:01 -0600

    added input validation to Swipe class

--- public_html/php/classes/Swipe.php
+++ public_html/php/classes/Swipe.php
@@ -9,9 +9,23 @@ class Swipe {
 	private $swipeStatus;
 
 	public function __construct(int $newSwipeId, int $newSwipeNumber, int $newSwipeStatus){
-		$this->setSwipeId($newSwipeId);
-		$this->setSwipeNumber($newSwipeNumber);
-		$this->setSwipeStatus($newSwipeStatus);
+		try {
+			$this->setSwipeId($newSwipeId);
+			$this->setSwipeNumber($newSwipeNumber);
+			$this->setSwipeStatus($newSwipeStatus);
+		}catch(\InvalidArgumentException $invalidArgumentException){
+			// rethrow exception to the caller
+			throw(new \InvalidArgumentException($invalidArgumentException->getMessage(),0,$invalidArgumentException));
+		} catch(\RangeException $rangeException){
+			// rethrow exception to the caller
+			throw(new \RangeException($rangeException->getMessage(),0,$rangeException));
+		} catch(\TypeError $typeError){
+			// rethrow exception to the caller
+			throw(new \TypeError($typeError->getMessage(),0,$typeError));
+		} catch(\Exception $exception){
+			// rethrow exception to the caller
+			throw(new \Exception($exception->getMessage(),0,$exception));
+		}
 	}
 
 	public function getSwipeId(){
@@ -27,14 +41,26 @@ class Swipe {
 	}
 
 	public function setSwipeId(int $newSwipeId){
+		// verify that newSwipeId is positive
+		if($newSwipeId <= 0){
+			throw new \RangeException("swipe id is not positive");
+		}
 		$this->swipeId = $newSwipeId;
 	}
 
 	public function setSwipeNumber(int $newSwipeNumber){
+		// verify that newSwipeNumber is positive
+		if($newSwipeNumber <= 0){
+			throw new \RangeException("swipe number is not positive");
+		}
 		$this->swipeNumber = $newSwipeNumber;
 	}
 
 	public function setSwipeStatus(int $newSwipeStatus){
+		// verify that newSwipeStatus is positive
+		if($newSwipeStatus <= 0){
+			throw new \RangeException("swipe status is not positive");
+		}
 		$this->swipeStatus = $newSwipeStatus;
 	}
 }
