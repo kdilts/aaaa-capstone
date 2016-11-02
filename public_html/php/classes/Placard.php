8181b2c44319a684dd50a976ff28deaaeabc9559
Trevor Pelton <tnpflyer20@msn.com>
2016-11-02 15:29:01 -0600

    started constructor for Placard

--- public_html/php/classes/Placard.php
+++ public_html/php/classes/Placard.php
@@ -1,13 +1,35 @@
 <?php
 
 class Placard {
+
+	/**
+	 * @var int $placardId
+	 */
 	private $placardId;
+
+	/**
+	 * @var int $placardStatus
+	 */
 	private $placardStatus;
+
+	/**
+	 * @var int $placardNumber
+	 */
 	private $placardNumber;
 
+	/**
+	 * Placard constructor.
+	 * @param int $newPlacardId
+	 * @param int $newPlacardStatus
+	 * @param int $newPlacardNumber
+	 */
 
-	public function __construct(int $placardId, int $placardStatus, int $placardNumber) {
-
+	public function __construct(int $newPlacardId, int $newPlacardStatus, int $newPlacardNumber) {
+		try {
+			$this->setPlacardId($newPlacardId);
+			$this->setPlacardNumber($newPlacardNumber);
+			$this->setPlacardStatus($newPlacardStatus);
+		}
 
 	}
 
@@ -35,21 +57,29 @@ class Placard {
 	/**
 	 * @param mixed $placardId
 	 */
-	public function setPlacardId($placardId) {
+	public function setPlacardId(int $newPlacardId) {
+		//checks if PlacardId is negative
+		if (newPlacardId <= 0) {
+			throw(new \RangeException("Placard ID cannot be negative."));
+		}
 		$this->placardId = $placardId;
 	}
 
 	/**
 	 * @param mixed $placardNumber
 	 */
-	public function setPlacardNumber($placardNumber) {
+	public function setPlacardNumber(int $newPlacardNumber) {
+		if (newPlacardNumber <= 0) {
+			throw(\RangeException("Placard Number cannot be negative."));
+		}
 		$this->placardNumber = $placardNumber;
 	}
 
 	/**
 	 * @param mixed $placardStatus
 	 */
-	public function setPlacardStatus($placardStatus) {
+	public function setPlacardStatus(int $newPlacardStatus) {
+		if (newPlaca)
 		$this->placardStatus = $placardStatus;
 	}
 }
