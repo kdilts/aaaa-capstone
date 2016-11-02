a73b11c5857dc34dacebe43b2c9dfaa13664475c
Maria Rosado <mrosado2@cnm.edu>
2016-11-02 15:29:04 -0600

    started class cohort

---
+++ public_html/php/classes/cohort.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * cohort class
+ *
+ * @author Maria Rosado <mrosado2@cnm.edu>
+ * @version 1.0.0
+ **/
+
+class cohort {
+		private $cohortId;
+	/**
+	 * id for this cohort; this is the primary key
+	 * @var int $cohortId
+	 **/
+	private $cohortApplicationId;
+
+
+
+}
