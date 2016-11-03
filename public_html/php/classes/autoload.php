9209792764424388482a2905cf2fab442d2fde97
Kevin Dilts <kdilts13@gmail.com>
2016-11-03 16:42:47 -0600

    fixed namespace in autoload

--- public_html/php/classes/autoload.php
+++ public_html/php/classes/autoload.php
@@ -15,7 +15,7 @@ spl_autoload_register(function($class) {
 	 * prefix: the prefix for all the classes (i.e., the namespace)
 	 * baseDir: the base directory for all classes (default = current directory)
 	 **/
-	$prefix = "Edu\\Cnm\\DdcAaaa\\Test";
+	$prefix = "Edu\\Cnm\\DdcAaaa";
 	$baseDir = __DIR__;
 
 	// does the class use the namespace prefix?
