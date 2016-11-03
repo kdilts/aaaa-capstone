151a8bf22866d24bfe6c156e6101ae4a057dff74
Maria Rosado <mrosado2@cnm.edu>
2016-11-03 00:52:18 -0600

    continued class cohort

--- public_html/php/classes/cohort.php
+++ public_html/php/classes/cohort.php
@@ -1,19 +1,64 @@
 <?php
+
+namespace Edu\Cnm\aaaa;
+
 /**
- * cohort class
+ * class Cohort for aaaa
  *
- * @author Maria Rosado <mrosado2@cnm.edu>
+ * @author Maria Rosado (mrosado2@cnm.edu)
+ *
  * @version 1.0.0
  **/
+class Cohort {
 
-class cohort {
-		private $cohortId;
 	/**
-	 * id for this cohort; this is the primary key
+	 * id for the cohort is the primary key
 	 * @var int $cohortId
 	 **/
-	private $cohortApplicationId;
+	private $cohortId;
 
+	/**
+	 * @var int $cohortApplicaionId
+	 **/
+	 private $cohortApplicationId;
+	/**
+	 * cohort constructor
+	 *
+	 * @param int $newcohortId
+	 * @param int $newcohortApplicationId
+	 * @throws \InvalidArgumentException
+	 * @throws \RangeException
+	 * @throws \Exception
+	 * @throws \TypeError
+	 **/
+	public function__construct(int @newCohortId = null, int $newCohortApplicationId) {
+		 try {
+			$this->setCohortId($newCohortId);
+			$this->setCohortAppplicationId($newCohortApplicationId);
+		} catch(\InvalidArgumentException $invalidArgumentException) {
+		// rethrow the exception to the caller
+		throw(new \InvalidArgumentException($invalidArgumentException->getmessage(), 0, $invalidArgument));
+		} catch(\RangeException $rangeException) {
+	// rethrow the exception to the caller
+		throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+		// rethrow the exception to the caller
+		throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+		// rethrow the exception to the caller
+		throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 
+	/**
+	 * accessor method for cohort id
+	 *
+	 * @return int|null value of cohort id
+	 **/
+	public function getCohortId(){
+		return($this->cohortId);
 
 }
+  }
+
+
