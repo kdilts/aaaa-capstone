4dc981459a8782955da7a3f459c4241ccf3311cd
Kevin Dilts <kdilts13@gmail.com>
2016-11-04 11:01:31 -0600

    created function definitions for Swipe.php
    https://app.asana.com/0/202206849274532/206913353199677

--- public_html/php/classes/StudentPermit.php
+++ public_html/php/classes/StudentPermit.php
@@ -8,4 +8,17 @@ class StudentPermit {
 	private $studentPermitCheckOutDate;
 	private $studentPermitCheckInDate;
 
+	public function __construct(int $newStudentPermitStudentId, int $newStudentPermitPlacardId, int $newStudentPermitSwipeId, \DateTime $newStudentPermitCheckOutDate, \DateTime $newStudentPermitCheckInDate){}
+
+	public function getStudentPermitStudentId(){}
+	public function getStudentPermitPlacardId(){}
+	public function getStudentPermitSwipeId(){}
+	public function getStudentPermitCheckOutDate(){}
+	public function getStudentPermitCheckInDate(){}
+
+	public function setStudentPermitStudentId(int $newStudentPermitStudentId){}
+	public function setStudentPermitPlacardId(int $newStudentPermitPlacardId){}
+	public function setStudentPermitSwipeId(int $newStudentPermitSwipeId){}
+	public function setStudentPermitCheckOutDate(\DateTime $newStudentPermitCheckOutDate){}
+	public function setStudentPermitCheckInDate(\DateTime $newStudentPermitCheckInDate){}
 }
