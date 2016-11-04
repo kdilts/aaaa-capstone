d793df663b51adedd901bc917101510bce53a75c
Kevin Dilts <kdilts13@gmail.com>
2016-11-04 10:08:35 -0600

    started to lay out the functions for Prospect.php
    https://app.asana.com/0/202206849274532/206913353199689

--- public_html/php/classes/Prospect.php
+++ public_html/php/classes/Prospect.php
@@ -3,4 +3,67 @@ namespace Edu\Cnm\DdcAaaa;
 
 class Prospect {
 
+	private $prospectId;
+	private $prospectCohortId;
+	private $prospectPhoneNumber;
+	private $prospectEmail;
+	private $prospectFirstName;
+	private $prospectLastName;
+
+	public function __construct(int $newProspectId, int $newProspectCohortId, string $newProspectPhoneNumber, string $newProspectEmail, string $newProspectFirstName, string $newProspectLastName){
+		$this->setProspectId($newProspectId);
+		$this->setProspectCohortId($newProspectCohortId);
+		$this->setProspectPhoneNumber($newProspectPhoneNumber);
+		$this->setProspectEmail($newProspectEmail);
+		$this->setProspectFirstName($newProspectFirstName);
+		$this->setProspectLastName($newProspectLastName);
+	}
+
+	public function getProspectId(){
+		return($this->getProspectId());
+	}
+
+	public function getProspectCohortId(){
+		return($this->getProspectCohortId());
+	}
+
+	public function getProspectPhoneNumber(){
+		return($this->getProspectPhoneNumber());
+	}
+
+	public function getProspectEmail(){
+		return($this->getProspectEmail());
+	}
+
+	public function getProspectFirstName(){
+		return($this->getProspectFirstName());
+	}
+
+	public function getProspectLastName(){
+		return($this->getProspectLastName());
+	}
+
+	public function setProspectId($newProspectId){
+		$this->prospectId=$newProspectId;
+	}
+
+	public function setProspectCohortId($newProspectCohortId){
+		$this->prospectCohortId=$newProspectCohortId;
+	}
+
+	public function setProspectPhoneNumber($newProspectPhoneNumber){
+		$this->prospectPhoneNumber=$newProspectPhoneNumber;
+	}
+
+	public function setProspectEmail($newProspectEmail){
+		$this->prospectEmail=$newProspectEmail;
+	}
+
+	public function setProspectFirstName($newProspectFirstName){
+		$this->prospectFirstName=$newProspectFirstName;
+	}
+
+	public function setProspectLastName($newProspectLastName){
+		$this->prospectLastName=$newProspectLastName;
+	}
 }
