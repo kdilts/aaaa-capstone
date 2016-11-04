e204d42c7640e508e7b73c2fc9e746df75dc5a9b
Kevin Dilts <kdilts13@gmail.com>
2016-11-04 11:21:51 -0600

    added input validation and doc blocks for Swipe.php added ValidateDate.php from bootcamp coders site
    https://app.asana.com/0/202206849274532/206913353199677

--- public_html/php/classes/StudentPermit.php
+++ public_html/php/classes/StudentPermit.php
@@ -2,23 +2,154 @@
 namespace Edu\Cnm\DdcAaaa;
 
 class StudentPermit {
+	use ValidateDate;
+
+	/**
+	 * @var int $studentPermitStudentId
+	 */
 	private $studentPermitStudentId;
+
+	/**
+	 * @var int $studentPermitPlacardId
+	 */
 	private $studentPermitPlacardId;
+
+	/**
+	 * @var int $studentPermitSwipeId
+	 */
 	private $studentPermitSwipeId;
+
+	/**
+	 * @var \DateTime $studentPermitCheckOutDate
+	 */
 	private $studentPermitCheckOutDate;
+
+	/**
+	 * @var \DateTime $studentPermitCheckInDate
+	 */
 	private $studentPermitCheckInDate;
 
-	public function __construct(int $newStudentPermitStudentId, int $newStudentPermitPlacardId, int $newStudentPermitSwipeId, \DateTime $newStudentPermitCheckOutDate, \DateTime $newStudentPermitCheckInDate){}
+	/**
+	 * StudentPermit constructor.
+	 * @param int $newStudentPermitStudentId
+	 * @param int $newStudentPermitPlacardId
+	 * @param int $newStudentPermitSwipeId
+	 * @param \DateTime $newStudentPermitCheckOutDate
+	 * @param \DateTime $newStudentPermitCheckInDate
+	 * @throws \InvalidArgumentException
+	 * @throws \RangeException
+	 * @throws \TypeError
+	 * @throws \Exception
+	 */
+	public function __construct(int $newStudentPermitStudentId, int $newStudentPermitPlacardId, int $newStudentPermitSwipeId, \DateTime $newStudentPermitCheckOutDate, \DateTime $newStudentPermitCheckInDate){
+		try{
+			$this->setStudentPermitStudentId($newStudentPermitStudentId);
+			$this->setStudentPermitPlacardId($newStudentPermitPlacardId);
+			$this->setStudentPermitSwipeId($newStudentPermitSwipeId);
+			$this->setStudentPermitCheckInDate($newStudentPermitCheckInDate);
+			$this->setStudentPermitCheckOutDate($newStudentPermitCheckOutDate);
+		}catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		}catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}catch(\TypeError $typeError) {
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		}catch(\Exception $exception) {
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 
-	public function getStudentPermitStudentId(){}
-	public function getStudentPermitPlacardId(){}
-	public function getStudentPermitSwipeId(){}
-	public function getStudentPermitCheckOutDate(){}
-	public function getStudentPermitCheckInDate(){}
+	/**
+	 * @return int
+	 */
+	public function getStudentPermitStudentId(){
+		return($this->studentPermitStudentId);
+	}
 
-	public function setStudentPermitStudentId(int $newStudentPermitStudentId){}
-	public function setStudentPermitPlacardId(int $newStudentPermitPlacardId){}
-	public function setStudentPermitSwipeId(int $newStudentPermitSwipeId){}
-	public function setStudentPermitCheckOutDate(\DateTime $newStudentPermitCheckOutDate){}
-	public function setStudentPermitCheckInDate(\DateTime $newStudentPermitCheckInDate){}
+	/**
+	 * @return int
+	 */
+	public function getStudentPermitPlacardId(){
+		return($this->studentPermitPlacardId);
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getStudentPermitSwipeId(){
+		return($this->studentPermitSwipeId);
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getStudentPermitCheckOutDate(){
+		return($this->studentPermitCheckOutDate);
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getStudentPermitCheckInDate(){
+		return($this->studentPermitCheckInDate);
+	}
+
+	/**
+	 * @param int $newStudentPermitStudentId
+	 */
+	public function setStudentPermitStudentId(int $newStudentPermitStudentId){
+		if ($newStudentPermitStudentId <= 0) {
+			throw(new \RangeException("Student Id cannot be negative."));
+		}
+		$this->studentPermitStudentId = $newStudentPermitStudentId;
+	}
+
+	/**
+	 * @param int $newStudentPermitPlacardId
+	 */
+	public function setStudentPermitPlacardId(int $newStudentPermitPlacardId){
+		if ($newStudentPermitPlacardId <= 0) {
+			throw(new \RangeException("Placard Id cannot be negative."));
+		}
+		$this->studentPermitPlacardId = $newStudentPermitPlacardId;
+	}
+
+	/**
+	 * @param int $newStudentPermitSwipeId
+	 */
+	public function setStudentPermitSwipeId(int $newStudentPermitSwipeId){
+		if ($newStudentPermitSwipeId <= 0) {
+			throw(new \RangeException("Swipe Id cannot be negative."));
+		}
+		$this->studentPermitSwipeId = $newStudentPermitSwipeId;
+	}
+
+	/**
+	 * @param \DateTime $newStudentPermitCheckOutDate
+	 */
+	public function setStudentPermitCheckOutDate(\DateTime $newStudentPermitCheckOutDate){
+		try {
+			$newStudentPermitCheckOutDate = self::validateDateTime($newStudentPermitCheckOutDate);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}
+
+		$this->studentPermitCheckOutDate = $newStudentPermitCheckOutDate;
+	}
+
+	/**
+	 * @param \DateTime $newStudentPermitCheckInDate
+	 */
+	public function setStudentPermitCheckInDate(\DateTime $newStudentPermitCheckInDate){
+		try {
+			$newStudentPermitCheckInDate = self::validateDateTime($newStudentPermitCheckInDate);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}
+		$this->studentPermitCheckInDate = $newStudentPermitCheckInDate;
+	}
 }
