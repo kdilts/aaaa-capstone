6af8665fa82d7c375359d3c52d4a9416a5369b44
Trevor Pelton <tnpflyer20@msn.com>
2016-11-04 10:30:26 -0600

    Added Status.php catches and throws. Need to see what throw to add to statusTypeName
    https://app.asana.com/0/202206849274532/206913353199691

--- public_html/php/classes/Status.php
+++ public_html/php/classes/Status.php
@@ -1,41 +1,57 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Fushi
- * Date: 11/4/2016
- * Time: 10:00 AM
- */
+
+namespace Edu\Cnm\DdcAaaa;
+
 class Status {
 	/**
-	 * @var int $statusTypeName
+	 * @var string $statusTypeName
 	 */
 	private $statusTypeName;
 	/**
 	 * @var int $statusTypeId
 	 */
-	private $statusTypeId
-	/**
-	 * @return int
-	 */
-	public function getStatusTypeId(): int {
-		return $this->statusTypeId;
+	private $statusTypeId;
+
+	public function __construct(string $newStatusTypeName, int $newStatusTypeId) {
+	try {
+		$this->setStatusTypeId($newStatusTypeId);
+		$this->statusTypeName($newStatusTypeName);
+	} catch(\InvalidArgumentException $invalidArgumentException){
+		throw(new \InvalidArgumentException($invalidArgumentException->getMessage(), 0, $invalidArgumentException));
+	} catch(\RangeException $rangeException){
+		throw(new \RangeException($rangeException->getMessage(), 0, $rangeException));
+	} catch(\TypeError $typeError){
+		throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+	} catch(\Exception $exception){
+		throw(new \Exception($exception->getMessage(), 0, $exception));
+	}
+
 	}
 	/**
 	 * @return int
 	 */
-	public function getStatusTypeName(): int {
+	public function getStatusTypeId(){
+		return $this->statusTypeId;
+	}
+	/**
+	 * @return string
+	 */
+	public function getStatusTypeName(){
 	return $this->statusTypeName;
 	}
 	/**
  	* @param int $statusTypeId
  	*/
 	public function setStatusTypeId(int $statusTypeId) {
+		if($statusTypeId <= 0) {
+			throw(new \RangeException("typeId can't be 0 or less."));
+		}
 	$this->statusTypeId = $statusTypeId;
 	}
 	/**
  	* @param int $statusTypeName
  	*/
-	public function setStatusTypeName(int $statusTypeName) {
+	public function setStatusTypeName(string $statusTypeName) {
 	$this->statusTypeName = $statusTypeName;
 }
 }
