9a37c8470ba65cfda3bf47b57d0f2860e0a1d095
Maria Rosado <mrosado2@cnm.edu>
2016-11-06 19:23:57 -0700

    added note PDO https://app.asana.com/0/202206849274532/206913353199685

--- public_html/php/classes/Note.php
+++ public_html/php/classes/Note.php
@@ -112,4 +112,57 @@ class Note {
 		}
 		$this->noteStudentId = $newNoteStudentId;
 	}
+
+	/**
+	 * @param \PDO $pdo
+	 * @throws \PDOException
+	 */
+	public function insert(\PDO $pdo) {
+		// enforce the noteId is null (i.e., don't insert a noteId that already exists)
+		if($this->noteId !== null) {
+			throw(new \PDOException("not a new noteId"));
+		}
+		// create query template
+		$query = "INSERT INTO note(noteId, noteStudentId, noteNoteTypeId, noteContent) VALUES(:noteId, :noteStudentId, noteNoteTypeId, :noteContent)";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holders in the template
+		$parameters = ["noteId" => $this->noteId, "noteStudentId" => $this->noteStudentId, "noteContent" => $this->noteContent];
+		$statement->execute($parameters);
+		// update the null noteId with what mySQL just gave us
+		$this->noteId = intval($pdo->lastInsertId());
+	}
+	/**
+	 * @param \PDO $pdo
+	 * @throws \PDOException
+	 */
+	public function delete(\PDO $pdo) {
+		// enforce the noteId is not null (i.e., don't delete a note that hasn't been inserted)
+		if($this->noteId === null) {
+			throw(new \PDOException("unable to delete a note that does not exist"));
+		}
+		// create query template
+		$query = "DELETE FROM note WHERE noteId = :noteId";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holder in the template
+		$parameters = ["notedId" => $this->noteId];
+		$statement->execute($parameters);
+	}
+	/**
+	 * @param \PDO $pdo
+	 * @throws \PDOException
+	 */
+	public function update(\PDO $pdo) {
+		// enforce the notedId is not null (i.e., don't update a note that hasn't been inserted)
+		if($this->noteId === null) {
+			throw(new \PDOException("unable to update a note that does not exist"));
+		}
+		// create query template
+		$query = "UPDATE note SET noteId = :noteId, noteStudentId = :noteStudentId, noteNoteTypeId = :noteNoteTypeId WHERE 
+		noteContent = :noteContent";
+		$statement = $pdo->prepare($query);
+		// bind the member variables to the place holders in the template
+		$parameters = ["noteId" => $this->noteId, "noteStudentId" => $this->noteStudentId, "noteNoteTypeId" =>
+			$this->noteNoteTypeId, "noteContent" => $this->noteContent];
+		$statement->execute($parameters);
+	}
 }
