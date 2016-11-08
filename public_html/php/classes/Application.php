a02269052fe0321f08c46446de89b2f7b23d4e7c
jeremiahwood <jzwood47@cnm.edu>
2016-11-08 15:58:56 -0700

    Set the constructor
    https://app.asana.com/0/202206849274532/206913353199679

--- public_html/php/classes/Application.php
+++ public_html/php/classes/Application.php
@@ -1,6 +1,7 @@
 <?php
-// TODO change your namespace to match the rest of the class namespaces - Edu\Cnm\DdcAaaa
-namespace Edu\Cnm\jwood47\aaaacapstone;
+namespace Edu\Cnm\aaaacapstone;
+
+use Edu\Cnm\DdcAaaa\ValidateDate;
 
 require_once ("autoload.php");
 
@@ -11,6 +12,7 @@ require_once ("autoload.php");
  **/
 
 class application {
+	use ValidateDate;
 	/**
 	 * @var int $applicationId
 	 */
@@ -68,6 +70,24 @@ class application {
 	 */
 	private $applicationUtmSource;
 
+	public function __construct(int $newApplicationId = null, string $newApplicationFirstName, string $newApplicationLastName, string $newApplicationEmail, string $newApplicationPhoneNumber, string $newApplicationSource, int $newApplicationCohortId, string $newApplicationAboutYou, string $newApplicationHopeToAccomplish, string $newApplicationExperience, string $newApplicationDateTime, string $newApplicationUtmCampaign, string $newApplicationUtmMedium, string $newApplicationUtmSource){
+		try {
+			$this->setApplicationId($newApplicationId);
+			$this->setApplicationFirstName($newApplicationFirstName);
+			$this->setApplicationLastName($newApplicationLastName);
+			$this->setApplicationEmail($newApplicationEmail);
+			$this->setApplicationPhoneNumber($newApplicationPhoneNumber);
+			$this->setApplicationSource($newApplicationSource);
+			$this->setApplicationCohortId($newApplicationCohortId);
+			$this->setApplicationAboutYou($newApplicationAboutYou);
+			$this->setApplicationHopeToAccomplish($newApplicationHopeToAccomplish);
+			$this->setApplicationExperience($newApplicationExperience);
+			$this->setApplicationDateTime($newApplicationDateTime);
+			$this->setApplicationUtmCampaign($newApplicationUtmCampaign);
+			$this->setApplicationUtmMedium($newApplicationUtmMedium);
+			$this->setApplicationUtmSource($newApplicationUtmSource);
+		}
+}
 	/**
 	 * @return int| null value of applicationId
 	 */
@@ -145,28 +165,26 @@ class application {
 		return $this->applicationDateTime;
 	}
 	/**
+	 * Application Constructor.
+	 * @param
+	 */
+	/**
 	 * @param int $newApplicationId
 	 * @throws \RangeException
 	 **/
 	public function setApplicationId(int $newApplicationId) {
-		// TODO should not assign to $this->applicationId before input validation
-		$this->applicationId = $newApplicationId;
 		//check if applicationId is negative
-		// TODO should not be using string validation for an integer
-		$newApplicationId = trim ($newApplicationId);
-		$newApplicationId = filter_var($newApplicationId, FILTER_FLAG_NO_ENCODE_QUOTES, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if($newApplicationId <= 0) {
 			throw(new \RangeException("Application Id cannot be negative."));
 		}
 		$this->applicationId = $newApplicationId;
 	}
+
 	/**
 	 * @param string $newApplicationFirstName
 	 * @throws \RangeException
 	 **/
 	public function setApplicationFirstName($newApplicationFirstName) {
-		// TODO should not assign to $this->application.. before input validation
-		$this->applicationFirstName = $newApplicationFirstName;
 		// verify first name is secure
 		$newApplicationFirstName = trim($newApplicationFirstName);
 		$newApplicationFirstName = filter_var($newApplicationFirstName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -180,27 +198,23 @@ class application {
 	 * @param string $newApplicationLastName
 	 */
 	public function setApplicationLastName(string $newApplicationLastName) {
-		// TODO should not assign to $this->application.. before input validation
-		$this->applicationLastName = $newApplicationLastName;
 		//verify last name is secure
 		$newApplicationLastName = trim($newApplicationLastName);
-		$newApplicationLastName = filter_var($newApplicationLastName, FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty ($newApplicationLastName) === true){
-			// TODO missing closing parenthesis - also copy pasted error message, should be last name not first name
-			throw(new \InvalidArgumentException("Application first name is empty or insecure");
+		$newApplicationLastName = filter_var($newApplicationLastName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty ($newApplicationLastName) === true) {
+			throw(new \InvalidArgumentException("Application last name is empty or insecure"));
 		}
 		$this->applicationLastName = $newApplicationLastName;
 	}
+
 	/**
 	 * @param string $newApplicationEmail
 	 * @throws \RangeException
 	 */
 	public function setApplicationEmail(string $newApplicationEmail) {
-		// TODO should not assign to $this->application.. before input validation
-		$this->applicationEmail = $newApplicationEmail;
 		$newApplicationEmail = trim($newApplicationEmail);
 		$newApplicationEmail = filter_var($newApplicationEmail, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty ($newApplicationEmail) === true){
+		if(empty ($newApplicationEmail) === true) {
 			throw (new \InvalidArgumentException("Application email is empty or secure"));
 		}
 		//verify email will fit in the database
@@ -208,135 +222,127 @@ class application {
 			throw(new \RangeException("application Email is to large"));
 		}
 		//store email
-		$this->applicationEmail=$newApplicationEmail;
+		$this->applicationEmail = $newApplicationEmail;
 	}
 
 	/**
 	 * @param string $newApplicationPhoneNumber
 	 */
 	public function setApplicationPhoneNumber(string $newApplicationPhoneNumber) {
-		// TODO should not assign to $this->application.. before input validation
-		$this->applicationPhoneNumber = $newApplicationPhoneNumber;
 		$newApplicationPhoneNumber = trim($newApplicationPhoneNumber);
 		$newApplicationPhoneNumber = filter_var($newApplicationPhoneNumber, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty ($newApplicationPhoneNumber) === true){
+		if(empty ($newApplicationPhoneNumber) === true) {
 			throw (new \InvalidArgumentException("Application phone number is empty or secure"));
 		}
 		//verify phone number will fit in the database
-		if(strlen($newApplicationPhoneNumber) >100);
-		throw (new \RangeException("application phone number is to large"));
-		// TODO store phone number outside of the function. bracket error
+		if(strlen($newApplicationPhoneNumber) > 100) {
+			throw (new \RangeException("application phone number is to large"));
+		}
+		//store phone number
+		$this->applicationPhoneNumber = $newApplicationPhoneNumber;
 	}
-	//store phone number
-$this->applicationPhoneNumber=$newApplicationPhoneNumber;
 
-
-/**
- * @param string $newApplicationSource
- */
-public function setApplicationSource (string $newApplicationSource){
-	// TODO should not assign to $this->application.. before input validation
-	$this->applicationSource = $newApplicationSource;
-	$newApplicationSource = trime ($newApplicationSource);
-	$newApplicationSource = filter_var($newApplicationSource, FILTER_SANITIZE_STRING,
-		FILTER_FLAG_NO_ENCODE_QUOTES);
-	if(empty ($newApplicationSource) === true){
-		throw (new \InvalidArgumentException("Application Source is empty or secure"));
-	}
-	//verify source will fit in the database
-	if(strlen($newApplicationSource) >1000);
-	throw (new \RangeException("Application source is to large"));
-	// TODO applicationSource outside of the function. bracket error
-}
+	/**
+	 * @param string $newApplicationSource
+	 */
+	public function setApplicationSource(string $newApplicationSource) {
+		$newApplicationSource = trim ($newApplicationSource);
+		$newApplicationSource = filter_var($newApplicationSource, FILTER_SANITIZE_STRING,
+			FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty ($newApplicationSource) === true) {
+			throw (new \InvalidArgumentException("Application Source is empty or secure"));
+		}
+		//verify source will fit in the database
+		if(strlen($newApplicationSource) > 1000) {
+			throw (new \RangeException("Application source is to large"));
+		}
 //store the source
-$this->applicationSource=$newApplicationSource;
-/**
- * @param string $newApplicationCohortId
- */
-public function setApplicationCohortId (string $newApplicationCohortId){
-	// TODO should not assign to $this->application.. before input validation
-	$this->applicationCohortId = $newApplicationCohortId;
-	$this->applicationCohortId = trim ($newApplicationCohortId);
-	$this->applicationCohortId = filter_var($newApplicationCohortId, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if (empty ($newApplicationCohortId) === true){
-		throw (new \InvalidArgumentException("Application Cohort Id is empty or secure"));
+		$this->applicationSource = $newApplicationSource;
 	}
-	//verify source will fit in the database
-	if (strlen ($newApplicationCohortId) >20);
-	throw (new \RangeException("Application Cohort Id is to large"));
-//store the Application Cohort Id
-	$this->applicationCohortId=$newApplicationCohortId;
-}
 
-public function setApplicationAboutYou (string $newApplicationAboutYou){
-	// TODO should not assign to $this->application.. before input validation
-	$this->applicationAboutYou = $newApplicationAboutYou;
-	$this->applicationAboutYou = trim ($newApplicationAboutYou);
-	$this->applicationAboutYou = filter($newApplicationAboutYou, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if (empty ($newApplicationAboutYou)=== true){
-		throw (new \InvalidArgumentException("application About You is empty or secure"));
+	/**
+	 * @param string $newApplicationCohortId
+	 */
+	public function setApplicationCohortId(string $newApplicationCohortId) {
+		// TODO should not assign to $this->application.. before input validation
+		$this->applicationCohortId = $newApplicationCohortId;
+		$this->applicationCohortId = trim($newApplicationCohortId);
+		$this->applicationCohortId = filter_var($newApplicationCohortId, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty ($newApplicationCohortId) === true) {
+			throw (new \InvalidArgumentException("Application Cohort Id is empty or secure"));
+		}
+		//verify source will fit in the database
+		if(strlen($newApplicationCohortId) > 20) {
+			throw (new \RangeException("Application Cohort Id is to large"));
+		}
+		//store the Application Cohort Id
+		$this->applicationCohortId = $newApplicationCohortId;
 	}
-	//verify source will fit in the database
-	if (strlen($newApplicationAboutYou) >1000);
-	throw (new \RangeException("application About You is to large"));
+
+	public function setApplicationAboutYou(string $newApplicationAboutYou) {
+		//$this->applicationAboutYou = trim ($newApplicationAboutYou);
+		$this->applicationAboutYou = filter_var($newApplicationAboutYou, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty ($newApplicationAboutYou) === true) {
+			throw (new \InvalidArgumentException("application About You is empty or secure"));
+		}
+		//verify source will fit in the database
+		if(strlen($newApplicationAboutYou) > 1000){
+		throw (new \RangeException("application About You is to large"));
+	}
 //store the Application About You
-	$this->applicationAboutYou=$newApplicationAboutYou;
-}
-
-/**
- * @param string $newApplicationHopeToAccomplish
- */
-public function setApplicationHopeToAccomplish (string $newApplicationHopeToAccomplish) {
-	// TODO should not assign to $this->application.. before input validation
-	$this->applicationHopeToAccomplish = $newApplicationHopeToAccomplish;
-	$this->applicationHopeToAccomplish = trim($newApplicationHopeToAccomplish);
-	$this->applicationHopeToAccomplish = filter($newApplicationHopeToAccomplish, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if(empty ($newApplicationHopeToAccomplish) === true) ; // TODO should have an open bracket here, not a semicolon
-	throw (new\InvalidArgumentException("application Hope to Accomplish is empty or secure"));
-}
-//verify source will fit in the datanase
-if (strlen($newApplicationHopeToAccomplish) >2000); // TODO should have an open bracket here, not a semicolon
-throw (new \RangeException("application Hope to Accomplish is to large"));
-//store the Application Hope To Accomplish
-$this->applicationHopeToAccomplish=$newApplicationHopeToAccomplish;
-} // TODO after you fix your if statement brackets, make sure all brackets are matched. this bracket should finish this function
-
-/**
- * @param string $ApplicationExperience
- */
-public function setApplicationExperience (string $newApplicationExperience){
-	// TODO should not assign to $this->application.. before input validation
-	$this->applicationExperience = $newApplicationExperience;
-	$this->applicationExperience = trim($newApplicationExperience);
-	$this->applicationExperience = filter_var($newApplicationExperience, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if (empty ($newApplicationExperience) === true);  // TODO should have an open bracket here, not a semicolon
-	throw (new\InvalidArgumentException("application Experience is empty or secure"));
-}
-//verify applcation experience will fir in the database
-if (strlen($newApplicationExperience) >2000);
-throw (new \RangeException("application experience is to large"));
-// store the Application Experience
-$this->applicationExperience = $newApplicationExperience;
-// TODO should have closing bracket here to finish function
-
-/**
- * @param string $newApplicationDateTime
- * @throws \InvalidArgumentException if $newApplicationDateTime is not a valid object or string
- * @throws \RangeException if $newApplicationDateTime is a date that does not exist
- */
-public function setApplicationDateTime(\DateTime $newApplicationDateTime){
-	try {
-		// TODO this looks good i think. you will need to add a 'use' statement at the top of this file to import ValidateDate.php
-		// TODO look at how i did it in StudentPermit.php - it is right under the 'class' line
-		$newApplicationDateTime = self::validateDateTime($newApplicationDateTime);
-	} catch(\InvalidArgumentException $invalidArgument) {
-		throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-	} catch(\RangeException $range) {
-		throw(new \RangeException($range->getMessage(), 0, $range));
+		$this->applicationAboutYou = $newApplicationAboutYou;
 	}
 
-	$this->applicationDateTime = $newApplicationDateTime;
+	/**
+	 * @param string $newApplicationHopeToAccomplish
+	 */
+	public function setApplicationHopeToAccomplish(string $newApplicationHopeToAccomplish) {
+		$this->applicationHopeToAccomplish = trim($newApplicationHopeToAccomplish);
+		$this->applicationHopeToAccomplish = filter_var($newApplicationHopeToAccomplish, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty ($newApplicationHopeToAccomplish) === true) {
+			throw (new\InvalidArgumentException("application Hope to Accomplish is empty or secure"));
+		}
+//verify source will fit in the datanase
+		if(strlen($newApplicationHopeToAccomplish) > 2000) {
+			throw (new \RangeException("application Hope to Accomplish is to large"));
+		}
+//store the Application Hope To Accomplish
+		$this->applicationHopeToAccomplish = $newApplicationHopeToAccomplish;
+	}
 
+	/**
+	 * @param string $ApplicationExperience
+	 */
+	public function setApplicationExperience(string $newApplicationExperience) {
+		$this->applicationExperience = trim($newApplicationExperience);
+		$this->applicationExperience = filter_var($newApplicationExperience, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty ($newApplicationExperience) === true) {
+			throw (new\InvalidArgumentException("application Experience is empty or secure"));
+		}
+//verify applcation experience will fir in the database
+		if(strlen($newApplicationExperience) > 2000) {
+			throw (new \RangeException("application experience is to large"));
+		}
+// store the Application Experience
+		$this->applicationExperience = $newApplicationExperience;
+	}
+
+	/**
+	 * @param string $newApplicationDateTime
+	 * @throws \InvalidArgumentException if $newApplicationDateTime is not a valid object or string
+	 * @throws \RangeException if $newApplicationDateTime is a date that does not exist
+	 */
+	public function setApplicationDateTime(\DateTime $newApplicationDateTime) {
+		try {
+			$newApplicationDateTime = self::validateDateTime($newApplicationDateTime);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		}
+
+		$this->applicationDateTime = $newApplicationDateTime;
+
+	}
 }
-
 
