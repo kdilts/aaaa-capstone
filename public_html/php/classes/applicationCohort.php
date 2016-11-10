8f6aabda6205aa56e5507f43e470ccce347062f5
Kevin Dilts <kdilts13@gmail.com>
2016-11-10 15:18:29 -0700

    adding getters and setters to applicationCohort class

--- public_html/php/classes/applicationCohort.php
+++ public_html/php/classes/applicationCohort.php
@@ -9,21 +9,37 @@ namespace Edu\Cnm\DdcAaaa;
  **/
 class applicationCohort implements \JsonSerializable {
 
-
+	/**
+	 * @var $applicationCohortId
+	 */
+	private $applicationCohortId;
 
 	/**
-	 * applicationCohort constructor
-	 *
+	 * @var $applicationCohortApplicationIdId
+	 */
+	private $applicationCohortApplicationId;
+
+	/**
+	 * @var $applicationCohortCohortId
+	 */
+	private $applicationCohortCohortId;
+
+	/**
+	 * applicationCohort constructor.
+	 * @param int|null $newApplicationCohortId
+	 * @param int $newApplicationCohortApplicationId
+	 * @param int $newApplicationCohortCohortId
 	 * @throws \InvalidArgumentException
 	 * @throws \RangeException
 	 * @throws \TypeError
 	 * @throws \Exception
-	 **/
-	public function __construct() {
+	 */
+	public function __construct(int $newApplicationCohortId = null, int $newApplicationCohortApplicationId, int $newApplicationCohortCohortId) {
 		try {
-
-		} catch(\InvalidArgumentException $invalidArgument)
-		{
+			$this->setApplicationCohortId($newApplicationCohortId);
+			$this->setApplicationCohortApplicationId($newApplicationCohortApplicationId);
+			$this->setApplicationCohortCohortId($newApplicationCohortCohortId);
+		} catch(\InvalidArgumentException $invalidArgument) {
 			// rethrow the exception to the caller
 			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0,$invalidArgument));
 		} catch(\RangeException $range) {
@@ -38,9 +54,53 @@ class applicationCohort implements \JsonSerializable {
 		}
 	}
 
+	/**
+	 * @return int
+	 */
+	private function getApplicationCohortId() {
+		return($this->applicationCohortId);
+	}
+
+	/**
+	 * @return int
+	 */
+	private function getApplicationCohortApplicationId() {
+		return($this->applicationCohortApplicationId);
+	}
+
+	/**
+	 * @return int
+	 */
+	private function getApplicationCohortCohortId() {
+		return($this->applicationCohortCohortId);
+	}
+	
+	/**
+	 * @param $newApplicationCohortId
+	 */
+	private function setApplicationCohortId($newApplicationCohortId) {
+		
+	}
+
+	/**
+	 * @param $newApplicationCohortApplicationId
+	 */
+	private function setApplicationCohortApplicationId($newApplicationCohortApplicationId) {
+		
+	}
+
+	/**
+	 * @param $newApplicationCohortCohortId
+	 */
+	private function setApplicationCohortCohortId($newApplicationCohortCohortId) {
+		
+	}
+
+	/**
+	 * @return array
+	 */
 	public function jsonSerialize() {
 		$fields = get_object_vars($this);
 		return($fields);
 	}
-
 }
