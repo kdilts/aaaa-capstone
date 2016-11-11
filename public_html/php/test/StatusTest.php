823e4dedcfb87c8bc83b8f96cc69c6621ead82ff
Maria Rosado <mrosado2@cnm.edu>
2016-11-11 14:07:45 -0700

    corrected errors still needs check warnings for insert valid, invalid and update for status https://app.asana.com/0/202206849274532/206902611499183

--- public_html/php/test/StatusTest.php
+++ public_html/php/test/StatusTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Edu\Cnm\DdcAaaa\Test;
+namespace Edu\Cnm\Aaaa\Test;
 
 use Edu\Cnm\DdcAaaa\{Status};
 
@@ -23,10 +23,7 @@ class Status extends AaaaTest {
 	 * @var string $VALID_STATUS
 	 **/
 	protected $VALID_STATUSTYPEID = "PHPUnit test passing";
-	/**
-	 * content of the updated
-	 * @var string $VALID_STATUSTYPENAME
-	 **/
+
 	protected $VALID_STATUSTYPENAME = "PHPUnit test still passing";
 
 	/**
@@ -44,172 +41,48 @@ class Status extends AaaaTest {
 			// count the number of rows and save it for later
 			$numRows = $this->getConnection()->getRowCount("status");
 
-			// create a new Status and inser to into mySQL
+			// create a new Status and insert to into mySQL
 			$status = new Status(null, $this->VALID_STATUSTYPEID, $this->VALID_STATUSTYPENAME);
 			$status->insert($this->getPDO());
 
 			// grab the data from mySQL and enforce the fields match our expectations
-			$pdoStatus = Status::getStatusByStatusTypeId($this->getPDO(), $syatus->getStatusTypeId());
-			$this->assetEqual($numRows + 1, $this->getConnection()->getRowCount("status"));
-			//$this->assertEqual(%pdoStatus->getStatusTypeId(), $this->VALID_STATUSTYPEID
-			$this->assertEqual(%pdoStatus->getStatusTypeName(), $this->VALID_STATUSTYPENAME);
+			$pdoStatus = Status::getStatusByStatusTypeId($this->getPDO(), $status->getStatusTypeId());
+			$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("status"));
+			//$this->assertEquals(%pdoStatus->getStatusTypeId(), $this->VALID_STATUSTYPEID);
+			$this->assertEquals($pdoStatus->getStatusTypeName(), $this->VALID_STATUSTYPENAME);
 			}
 
 	/**
-	 * test inserting a Status and verify that it already exists
+	 * test inserting a Status that already exists
 	 * @expectedException \PDOException
 	 **/
-	public function testInsertValidStatus() {
+	public function testInsertInvalidStatus() {
 		// create a Status with a non null status id and watch it fail
-		$status = new Status(AaaaTest::
-		:
-		INVALID_KEY, $this->VALID_STATUSTYPEID, $this->VALID_STATUTYPENAME);
-		$status->insert($this->getPDO());
-		}
-		/**
-		 * test inserting a Status, editin it, and then updating it
-		 **/
+		$status = new Status(AaaaTest::INVALID_KEY, $this->VALID_STATUSTYPEID, $this->VALID_STATUSTYPENAME);
+		$status->insert($this->gerPDO());
+	}
+
+	/**
+	 * test inserting a Status, editing it, and then updating it
+	 **/
 	public function testUpdateValidStatus() {
-
 		// count the number of rows and save it for later
-		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
-		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
-		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
-		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
+		$numRows = $this->getConnection()->getRowCount("status");
+
+		// create a new Status and insert to into mySQL
+		$status = new Status(null, $this->VALID_STATUSTYPEID, $this->VALID_STATUSTYPENAME);
+		$status->insert($this->getPDO());
+
+		//edit the Status and update it in mySQL
+		$status->setStatusTypeId($this->VALID_STATUSTYPEID2);
+		$status->update($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our
+		$pdoStatus = Status::getStatusByStatusTypeId($this->getPDO(), $status->getStatusTypeId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("status"));
+		$this->assertEquals($pdoStatus->getStatusTypeId(), $this->VALID_STATUSTYPEID2);
+		$this->assertEquals($pdoStatus->getStatusTypeName(), $this->VALID_STATUSTYPENAME);
+
 	}
 
-	/**
-	 * test inserting a Tweet that already exists
-	 *
-	 * @expectedException PDOException
-	 **/
-	public function testInsertInvalidTweet() {
-		// create a Tweet with a non null tweet id and watch it fail
-		$tweet = new Tweet(DataDesignTest::INVALID_KEY, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->insert($this->getPDO());
-	}
-
-	/**
-	 * test inserting a Tweet, editing it, and then updating it
-	 **/
-	public function testUpdateValidTweet() {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("tweet");
-
-		// create a new Tweet and insert to into mySQL
-		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->insert($this->getPDO());
-
-		// edit the Tweet and update it in mySQL
-		$tweet->setTweetContent($this->VALID_TWEETCONTENT2);
-		$tweet->update($this->getPDO());
-
-		// grab the data from mySQL and enforce the fields match our expectations
-		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
-		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
-		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT2);
-		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
-	}
-
-	/**
-	 * test updating a Tweet that does not exist
-	 *
-	 * @expectedException PDOException
-	 **/
-	public function testUpdateInvalidTweet() {
-		// create a Tweet, try to update it without actually updating it and watch it fail
-		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->update($this->getPDO());
-	}
-
-	/**
-	 * test creating a Tweet and then deleting it
-	 **/
-	public function testDeleteValidTweet() {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("tweet");
-
-		// create a new Tweet and insert to into mySQL
-		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->insert($this->getPDO());
-
-		// delete the Tweet from mySQL
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
-		$tweet->delete($this->getPDO());
-
-		// grab the data from mySQL and enforce the Tweet does not exist
-		$pdoTweet = Tweet::getTweetByTweetId($this->getPDO(), $tweet->getTweetId());
-		$this->assertNull($pdoTweet);
-		$this->assertEquals($numRows, $this->getConnection()->getRowCount("tweet"));
-	}
-
-	/**
-	 * test deleting a Tweet that does not exist
-	 *
-	 * @expectedException PDOException
-	 **/
-	public function testDeleteInvalidTweet() {
-		// create a Tweet and try to delete it without actually inserting it
-		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->delete($this->getPDO());
-	}
-
-	/**
-	 * test grabbing a Tweet by tweet content
-	 **/
-	public function testGetValidTweetByTweetContent() {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("tweet");
-
-		// create a new Tweet and insert to into mySQL
-		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->insert($this->getPDO());
-
-		// grab the data from mySQL and enforce the fields match our expectations
-		$results = Tweet::getTweetByTweetContent($this->getPDO(), $tweet->getTweetContent());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
-		$this->assertCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Dmcdonald21\\DataDesign\\Tweet", $results);
-
-		// grab the result from the array and validate it
-		$pdoTweet = $results[0];
-		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
-		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
-		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
-	}
-
-	/**
-	 * test grabbing a Tweet by content that does not exist
-	 **/
-	public function testGetInvalidTweetByTweetContent() {
-		// grab a tweet by searching for content that does not exist
-		$tweet = Tweet::getTweetByTweetContent($this->getPDO(), "you will find nothing");
-		$this->assertCount(0, $tweet);
-	}
-
-	/**
-	 * test grabbing all Tweets
-	 **/
-	public function testGetAllValidTweets() {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("tweet");
-
-		// create a new Tweet and insert to into mySQL
-		$tweet = new Tweet(null, $this->profile->getProfileId(), $this->VALID_TWEETCONTENT, $this->VALID_TWEETDATE);
-		$tweet->insert($this->getPDO());
-
-		// grab the data from mySQL and enforce the fields match our expectations
-		$results = Tweet::getAllTweets($this->getPDO());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
-		$this->assertCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Dmcdonald21\\DataDesign\\Tweet", $results);
-
-		// grab the result from the array and validate it
-		$pdoTweet = $results[0];
-		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
-		$this->assertEquals($pdoTweet->getTweetContent(), $this->VALID_TWEETCONTENT);
-		$this->assertEquals($pdoTweet->getTweetDate(), $this->VALID_TWEETDATE);
-	}
 }
