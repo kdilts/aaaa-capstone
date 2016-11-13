0f78ba4782b690d0db55d64db23978274e85cb23
Trevor Pelton <tnpflyer20@msn.com>
2016-11-13 14:39:04 -0700

    Added PDO getters for bridge. Needs work on a few warnings, and a double check of functionality.

--- public_html/php/classes/Bridge.php
+++ public_html/php/classes/Bridge.php
@@ -171,6 +171,124 @@ namespace Edu\Cnm\DdcAaaa;
 				$this->bridgeUserName];
 			$statement->execute($parameters);
 		}
+		public static function getBridgeByBridgeStaffId(\PDO $pdo, int $bridgeStaffId) {
+			// sanitize the bridgeId before searching
+			if($bridgeStaffId <= 0) {
+				throw(new \PDOException("bridgeStaffId not positive"));
+			}
+
+			// create query template
+			$query = "SELECT bridgeStaffId, bridgeName, bridgeUserName FROM bridge WHERE bridgeStaffId = :bridgeStaffId";
+			$statement = $pdo->prepare($query);
+
+			// bind the bridgeStaff id to the place holder in template
+			$parameters = ["bridgeStaffId" => $bridgeStaffId];
+			$statement->execute($parameters);
+
+			// grab bridge from SQL
+			try {
+				$bridge = null;
+				$statement->setFetchMode(\PDO::FETCH_ASSOC);
+				$row = $statement->fetch();
+				if($row !== false) {
+					$bridge = new bridge($row["bridgeStaffId"], $row["bridgeName"], $row["bridgeUserName"]);
+				}
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+			return ($bridge);
+		}
+
+		/
+		public static function getBridgeByBridgeName(\PDO $pdo, int $bridgeName) {
+			// sanitize the bridgeName before searching
+			if($bridgeName === null) {
+				throw(new \PDOException("bridgeName is empty/")); //is this right?
+			}
+
+			// create query template
+			$query = "SELECT bridgeStaffId, bridgeName, bridgeUserName FROM bridge WHERE bridgeName = :bridgeName";
+			$statement = $pdo->prepare($query);
+
+			// bind the bridge name to the place holder in template
+			$parameters = ["bridgeName" => $bridgeName];
+			$statement->execute($parameters);
+
+			// build an array of bridges
+			$bridges = new \SplFixedArray($statement->rowCount());
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			while(($row = $statement->fetch()) !== false) {
+				try {
+					$bridge = new Bridge($row["bridgeStaffId"], $row["bridgeName"], $row["bridgeUserName"]);
+					$bridges[$bridges->key()] = $bridge;
+					$bridges->next();
+				} catch(\Exception $exception) {
+					// if the row couldn't be converted, rethrow it
+					throw(new \PDOException($exception->getMessage(), 0, $exception));
+				}
+			}
+			return $bridges;
+		}
+
+
+		public static function getBridgeByBridgeUserName(\PDO $pdo, string $bridgeUserName) {
+			// sanitize the bridgeUserName before searching
+			if($bridgeUserName === null) {
+				throw(new \PDOException("bridgeUserName can't be empty"));
+			}
+
+			// create query template
+			$query = "SELECT bridgeStaffId, bridgeName, bridgeUserName FROM bridge WHERE bridgeUserName = :bridgeUserName";
+			$statement = $pdo->prepare($query);
+
+			// bind the bridgeUserName to the place holder in template
+			$parameters = ["bridgeUserName" => $bridgeUserName];
+			$statement->execute($parameters);
+
+			// build an array of bridges
+			$bridges = new \SplFixedArray($statement->rowCount());
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			while(($row = $statement->fetch()) !== false) {
+				try {
+					$bridge = new Bridge($row["bridgeStaffId"], $row["bridgeName"], $row["bridgeUserName"]);
+					$bridges[$bridges->key()] = $bridge;
+					$bridges->next();
+				} catch(\Exception $exception) {
+					// if the row couldn't be converted, rethrow it
+					throw(new \PDOException($exception->getMessage(), 0, $exception));
+				}
+			}
+			return $bridges;
+		}
+
+		/**
+		 * @param \PDO $pdo connection objects
+		 * @return \SplFixedArray SplFi
+		 * @throws \PDOException when mySQL related errors occur
+		 * @throws \TypeError when variables are not the correct data type
+		 */
+		public static function getAllbridges(\PDO $pdo) {
+			// create query template
+			$query = "SELECT bridgeStaffId, bridgeName, bridgeUserName FROM bridge";
+			$statement = $pdo->prepare($query);
+			$statement->execute();
+
+			// build an array of bridges
+			$bridges = new \SplFixedArray($statement->rowCount());
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			while(($row = $statement->fetch()) !== false) {
+				try {
+					$bridge = new Bridge($row["bridgeStaffId"], $row["bridgeName"], $row["bridgeUserName"]);
+					$bridges[$bridges->key()] = $bridge;
+					$bridges->next();
+				} catch(\Exception $exception) {
+					// if the row couldn't be converted, rethrow it
+					throw(new \PDOException($exception->getMessage(), 0, $exception));
+				}
+			}
+			return $bridges;
+		}
 	}
 
 
