6a76f45ea979073b947cef45fb131e26207a416f
Kevin Dilts <kdilts13@gmail.com>
2016-11-14 08:39:03 -0700

    added json serialize to statusType class

--- public_html/php/classes/StatusType.php
+++ public_html/php/classes/StatusType.php
@@ -13,18 +13,18 @@ class StatusType {
 	private $statusTypeId;
 
 	public function __construct(string $newStatusTypeName, int $newStatusTypeId) {
-	try {
-		$this->setStatusTypeId($newStatusTypeId);
-		$this->setStatusTypeName($newStatusTypeName);
-	} catch(\InvalidArgumentException $invalidArgumentException){
-		throw(new \InvalidArgumentException($invalidArgumentException->getMessage(), 0, $invalidArgumentException));
-	} catch(\RangeException $rangeException){
-		throw(new \RangeException($rangeException->getMessage(), 0, $rangeException));
-	} catch(\TypeError $typeError){
-		throw(new \TypeError($typeError->getMessage(), 0, $typeError));
-	} catch(\Exception $exception){
-		throw(new \Exception($exception->getMessage(), 0, $exception));
-	}
+		try {
+			$this->setStatusTypeId($newStatusTypeId);
+			$this->setStatusTypeName($newStatusTypeName);
+		} catch(\InvalidArgumentException $invalidArgumentException){
+			throw(new \InvalidArgumentException($invalidArgumentException->getMessage(), 0, $invalidArgumentException));
+		} catch(\RangeException $rangeException){
+			throw(new \RangeException($rangeException->getMessage(), 0, $rangeException));
+		} catch(\TypeError $typeError){
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception){
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
 
 	}
 	/**
@@ -40,42 +40,50 @@ class StatusType {
 		return $this->statusTypeName;
 	}
 	/**
- 	* @param int $statusTypeId
- 	*/
+	 * @param int $statusTypeId
+	 */
 	public function setStatusTypeId(int $statusTypeId) {
 		if($statusTypeId <= 0) {
 			throw(new \RangeException("typeId can't be 0 or less."));
 		}
-	$this->statusTypeId = $statusTypeId;
+		$this->statusTypeId = $statusTypeId;
 	}
 	/**
- 	* @param string $newStatusTypeName
- 	*/
+	 * @param string $newStatusTypeName
+	 */
 	public function setStatusTypeName(string $newStatusTypeName) {
 		$newStatusTypeName = trim($newStatusTypeName);
 		$newStatusTypeName = filter_var($newStatusTypeName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newStatusTypeName) === true) {
 			throw (new \InvalidArgumentException("Status type name is either empty or insecure."));
 		}
-	$this->statusTypeName = $newStatusTypeName;
-}
+		$this->statusTypeName = $newStatusTypeName;
+	}
 
 	/**
 	 * @param \PDO $pdo
 	 */
-public function insert(\PDO $pdo) {
-	if($this->statusTypeId === null) {
-		throw(new \PDOException("Need a status type."));
+	public function insert(\PDO $pdo) {
+		if($this->statusTypeId === null) {
+			throw(new \PDOException("Need a status type."));
+		}
+		$query = "INSERT INTO statusType(statusTypeId, statusTypeName) VALUES(:statusTypeId, :statusTypeName)";
+		$statement = $pdo->prepare($query);
+
+
+		$parameters = ["statusTypeId" => $this->statusTypeId, "statusTypeName" => $this->statusTypeName];
+		$statement->execute($parameters);
+
+
+		$this->statusTypeId = intval($pdo->lastInsertId());
+
 	}
-	$query = "INSERT INTO statusType(statusTypeId, statusTypeName) VALUES(:statusTypeId, :statusTypeName)";
-	$statement = $pdo->prepare($query);
 
-
-	$parameters = ["statusTypeId" => $this->statusTypeId, "statusTypeName" => $this->statusTypeName];
-	$statement->execute($parameters);
-
-
-	$this->statusTypeId = intval($pdo->lastInsertId());
-
-}
+	/**
+	 * @return array
+	 */
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		return($fields);
+	}
 }
