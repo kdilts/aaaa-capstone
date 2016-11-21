df10a41530d9781ca5b5b8d4e26c919a3e632a72
Trevor Pelton <tnpflyer20@msn.com>
2016-11-21 16:02:35 -0700

    trying fix

--- public_html/php/test/ApplicationTest.php
+++ public_html/php/test/ApplicationTest.php
@@ -84,75 +84,75 @@ class ApplicationTest extends AaaaTest {
 	 *
 	 * @expectedException \PDOException
 	 **/
-//	public function testInsertInvalidApplication() {
-//		// create a Application with a non null application id and watch it fail
-//		$application = new Application(AaaaTest::INVALID_KEY, $this->VALID_APPLICATIONFIRSTNAME, $this->VALID_APPLICATIONLASTNAME, $this->VALID_APPLICATIONEMAIL,
-//			$this->VALID_APPLICATIONPHONENUMBER, $this->VALID_APPLICATIONSOURCE, $this->VALID_APPLICATIONABOUTYOU, $this->VALID_APPLICATIONHOPETOACCOMPLISH, $this->VALID_APPLICATIONEXPERIENCE, $this->VALID_APPLICATIONDATETIME, $this->VALID_APPLICATIONUTMCAMPAIGN, $this->VALID_APPLICATIONUTMMEDIUM, $this->VALID_APPLICATIONUTMSOURCE);
-//		$application->insert($this->getPDO());
-//	}
-//
-//	/**
-//	 * test inserting an Application , editing it, and then updating it
-//	 **/
-//
-//
-//	/**
-//	 * test grabbing a Application by Application content
-//	 **/
-//	public function testGetValidApplicationByApplicationId() {
-//		// count the number of rows and save it for later
-//		$numRows = $this->getConnection()->getRowCount("application");
-//
-//		// create a new Application and insert to into mySQL
-//		$application = new Application(null, $this->VALID_APPLICATIONFIRSTNAME, $this->VALID_APPLICATIONLASTNAME, $this->VALID_APPLICATIONEMAIL,
-//			$this->VALID_APPLICATIONPHONENUMBER, $this->VALID_APPLICATIONSOURCE, $this->VALID_APPLICATIONABOUTYOU, $this->VALID_APPLICATIONHOPETOACCOMPLISH, $this->VALID_APPLICATIONEXPERIENCE, $this->VALID_APPLICATIONDATETIME, $this->VALID_APPLICATIONUTMCAMPAIGN, $this->VALID_APPLICATIONUTMMEDIUM, $this->VALID_APPLICATIONUTMSOURCE);
-//		$application->insert($this->getPDO());
-//
-//		// grab the data from mySQL and enforce the fields match our expectations
-//		$results = Application::getApplicationByApplicationId($this->getPDO(), $application->getApplicationId());
-//		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("application"));
-//		$this->assertNotNull($results);
-//		$this->assertInstanceOf("Edu\\Cnm\\DdcAaaa\\Application", $results);
-//
-//		// grab the result from the array and validate it
-//		$pdoApplication = $results;
-//		$this->assertEquals($pdoApplication->getApplicationId(), $application->getApplicationId());
-//		$this->assertEquals($pdoApplication->getApplicationFirstName(), $this->VALID_APPLICATIONFIRSTNAME);
-//		$this->assertEquals($pdoApplication->getApplicationLastName(), $this->VALID_APPLICATIONLASTNAME);
-//	}
-//
-//	/**
-//	 * test grabbing a Application by content that does not exist
-//	 **/
-//	public function testGetInvalidApplicationByApplicationId() {
-//		// grab a Application by searching for content that does not exist
-//		$application = Application::getApplicationByApplicationId($this->getPDO(), AaaaTest::INVALID_KEY);
-//		$this->assertNull($application);
-//	}
-//	/**
-//	 * test grabbing all ApplicationId
-//	 **/
-//	public function testGetAllValidApplications() {
-//		// count the number of rows and save it for later
-//		$numRows = $this->getConnection()->getRowCount("application");
-//
-//		// create a new Application and insert to into mySQL
-//		$application = new Application(null, $this->VALID_APPLICATIONFIRSTNAME, $this->VALID_APPLICATIONLASTNAME, $this->VALID_APPLICATIONEMAIL,
-//			$this->VALID_APPLICATIONPHONENUMBER, $this->VALID_APPLICATIONSOURCE, $this->VALID_APPLICATIONABOUTYOU, $this->VALID_APPLICATIONHOPETOACCOMPLISH, $this->VALID_APPLICATIONEXPERIENCE, $this->VALID_APPLICATIONDATETIME, $this->VALID_APPLICATIONUTMCAMPAIGN, $this->VALID_APPLICATIONUTMMEDIUM, $this->VALID_APPLICATIONUTMSOURCE);
-//		$application->insert($this->getPDO());
-//
-//		// grab the data from mySQL and enforce the fields match our expectations
-//		$results = Application::getAllApplications($this->getPDO());
-//		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("application"));
-//		$this->assertCount(1, $results);
-//		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DdcAaaa\\Application", $results);
-//
-//		// grab the result from the array and validate it
-//		$pdoApplication = $results[0];
-//		$this->assertEquals($pdoApplication->getApplicationId(), $application->getApplicationId());
-//		$this->assertEquals($pdoApplication->getApplicationFirstName(), $this->VALID_APPLICATIONFIRSTNAME);
-//		$this->assertEquals($pdoApplication->getApplicationLastName(), $this->VALID_APPLICATIONLASTNAME);
-//	}
+	public function testInsertInvalidApplication() {
+		// create a Application with a non null application id and watch it fail
+		$application = new Application(AaaaTest::INVALID_KEY, $this->VALID_APPLICATIONFIRSTNAME, $this->VALID_APPLICATIONLASTNAME, $this->VALID_APPLICATIONEMAIL,
+			$this->VALID_APPLICATIONPHONENUMBER, $this->VALID_APPLICATIONSOURCE, $this->VALID_APPLICATIONABOUTYOU, $this->VALID_APPLICATIONHOPETOACCOMPLISH, $this->VALID_APPLICATIONEXPERIENCE, $this->VALID_APPLICATIONDATETIME, $this->VALID_APPLICATIONUTMCAMPAIGN, $this->VALID_APPLICATIONUTMMEDIUM, $this->VALID_APPLICATIONUTMSOURCE);
+		$application->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting an Application , editing it, and then updating it
+	 **/
+
+
+	/**
+	 * test grabbing a Application by Application content
+	 **/
+	public function testGetValidApplicationByApplicationId() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("application");
+
+		// create a new Application and insert to into mySQL
+		$application = new Application(null, $this->VALID_APPLICATIONFIRSTNAME, $this->VALID_APPLICATIONLASTNAME, $this->VALID_APPLICATIONEMAIL,
+			$this->VALID_APPLICATIONPHONENUMBER, $this->VALID_APPLICATIONSOURCE, $this->VALID_APPLICATIONABOUTYOU, $this->VALID_APPLICATIONHOPETOACCOMPLISH, $this->VALID_APPLICATIONEXPERIENCE, $this->VALID_APPLICATIONDATETIME, $this->VALID_APPLICATIONUTMCAMPAIGN, $this->VALID_APPLICATIONUTMMEDIUM, $this->VALID_APPLICATIONUTMSOURCE);
+		$application->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Application::getApplicationByApplicationId($this->getPDO(), $application->getApplicationId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("application"));
+		$this->assertNotNull($results);
+		$this->assertInstanceOf("Edu\\Cnm\\DdcAaaa\\Application", $results);
+
+		// grab the result from the array and validate it
+		$pdoApplication = $results;
+		$this->assertEquals($pdoApplication->getApplicationId(), $application->getApplicationId());
+		$this->assertEquals($pdoApplication->getApplicationFirstName(), $this->VALID_APPLICATIONFIRSTNAME);
+		$this->assertEquals($pdoApplication->getApplicationLastName(), $this->VALID_APPLICATIONLASTNAME);
+	}
+
+	/**
+	 * test grabbing a Application by content that does not exist
+	 **/
+	public function testGetInvalidApplicationByApplicationId() {
+		// grab a Application by searching for content that does not exist
+		$application = Application::getApplicationByApplicationId($this->getPDO(), AaaaTest::INVALID_KEY);
+		$this->assertNull($application);
+	}
+	/**
+	 * test grabbing all ApplicationId
+	 **/
+	public function testGetAllValidApplications() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("application");
+
+		// create a new Application and insert to into mySQL
+		$application = new Application(null, $this->VALID_APPLICATIONFIRSTNAME, $this->VALID_APPLICATIONLASTNAME, $this->VALID_APPLICATIONEMAIL,
+			$this->VALID_APPLICATIONPHONENUMBER, $this->VALID_APPLICATIONSOURCE, $this->VALID_APPLICATIONABOUTYOU, $this->VALID_APPLICATIONHOPETOACCOMPLISH, $this->VALID_APPLICATIONEXPERIENCE, $this->VALID_APPLICATIONDATETIME, $this->VALID_APPLICATIONUTMCAMPAIGN, $this->VALID_APPLICATIONUTMMEDIUM, $this->VALID_APPLICATIONUTMSOURCE);
+		$application->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Application::getAllApplications($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("application"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DdcAaaa\\Application", $results);
+
+		// grab the result from the array and validate it
+		$pdoApplication = $results[0];
+		$this->assertEquals($pdoApplication->getApplicationId(), $application->getApplicationId());
+		$this->assertEquals($pdoApplication->getApplicationFirstName(), $this->VALID_APPLICATIONFIRSTNAME);
+		$this->assertEquals($pdoApplication->getApplicationLastName(), $this->VALID_APPLICATIONLASTNAME);
+	}
 
 
 }
