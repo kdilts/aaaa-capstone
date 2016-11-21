89e186bd421c093a66c840e4702d48cd421151ed
Kevin Dilts <kdilts13@gmail.com>
2016-11-20 22:34:07 -0700

    copied applicationCohort tests over to prospectCohort

--- public_html/php/test/ProspectCohortTest.php
+++ public_html/php/test/ProspectCohortTest.php
@@ -10,7 +10,7 @@ require_once("AaaaTest.php");
 require_once(dirname(__DIR__) . "/classes/autoload.php");
 
 /**
- * Full PHPUnit test for the Placard class
+ * Full PHPUnit test for the Prospect class
  *
  * This is a complete PHPUnit test of the Prospect class.
  *
@@ -29,17 +29,11 @@ class ProspectCohortTest extends AaaaTest {
 		// run the default setUp() method first
 		parent::setUp();
 
-		// create date
-		$date = new \DateTime();
-		//$date = $date->format("Y-m-d H:i:s");
-
 		// create cohort
 		$this->cohort = new Cohort(null, 1);
 		$this->cohort->insert($this->getPDO());
 
-
-//int $newProspectId = null, int $newProspectCohortId, string $newProspectPhoneNumber, string $newProspectEmail, string $newProspectFirstName, string $newProspectLastName
-		// create a prospect
+		// create an prospect
 		$this->prospect = new Prospect(null, $this->cohort->getCohortId(), "555-555-5555", "em@ail.com", "john", "doe");
 		$this->prospect->insert($this->getPDO());
 	}
@@ -68,8 +62,120 @@ class ProspectCohortTest extends AaaaTest {
 	 * @expectedException \PDOException
 	 **/
 	public function testInsertInvalidProspectCohort() {
-		// create a Placard with a non null placard id and watch it fail
+		// create a Placard with a non null prospectCohort id and watch it fail
 		$placard = new ProspectCohort(AaaaTest::INVALID_KEY, $this->prospect->getProspectId(), $this->cohort->getCohortId());
 		$placard->insert($this->getPDO());
 	}
+
+	/**
+	 *  test grabbing an ProspectCohort by ProspectCohortId
+	 */
+	public function testGetValidProspectCohortByProspectCohortId(){
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("prospectCohort");
+
+		// create a new ProspectCohort and insert to into mySQL
+		$prospectCohort = new ProspectCohort(null, $this->prospect->getProspectId(), $this->cohort->getCohortId());
+		$prospectCohort->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProspectCohort = ProspectCohort::getProspectCohortByProspectCohortId($this->getPDO(), $prospectCohort->getProspectCohortId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("prospectCohort"));
+		$this->assertEquals($pdoProspectCohort->getProspectCohortProspectId(), $this->prospect->getProspectId());
+		$this->assertEquals($pdoProspectCohort->getProspectCohortCohortId(), $this->cohort->getCohortId());
+	}
+
+	/**
+	 *  test grabbing an ProspectCohort by ProspectCohortId that does not exist
+	 */
+	public function testGetInvalidProspectCohortByProspectCohortId(){
+		// grab a prospectCohort by searching for id that does not exist
+		$prospectCohort = ProspectCohort::getProspectCohortByProspectCohortId($this->getPDO(), AaaaTest::INVALID_KEY);
+		$this->assertNull($prospectCohort);
+	}
+
+	/**
+	 *  test grabbing ProspectCohorts by ProspectCohortCohortId
+	 */
+	public function testGetValidProspectCohortByCohortId(){
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("prospectCohort");
+
+		// create a new ProspectCohort and insert to into mySQL
+		$prospectCohort = new ProspectCohort(null, $this->prospect->getProspectId(), $this->cohort->getCohortId());
+		$prospectCohort->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = ProspectCohort::getProspectCohortByCohortId($this->getPDO(), $prospectCohort->getProspectCohortCohortId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("prospectCohort"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DdcAaaa\\ProspectCohort", $results);
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProspectCohort = $results[0];
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("prospectCohort"));
+		$this->assertEquals($pdoProspectCohort->getProspectCohortProspectId(), $this->prospect->getProspectId());
+		$this->assertEquals($pdoProspectCohort->getProspectCohortCohortId(), $this->cohort->getCohortId());
+	}
+
+	/**
+	 *  test grabbing ProspectCohorts by ProspectCohortCohortId that does not exit
+	 */
+	public function testGetInvalidProspectCohortByCohortId(){
+		// grab a prospectCohort by searching for id that does not exist
+		$prospectCohort = ProspectCohort::getProspectCohortByCohortId($this->getPDO(), AaaaTest::INVALID_KEY);
+		$this->assertCount(0, $prospectCohort);
+	}
+
+	/**
+	 *  test grabbing an ProspectCohort by ProspectCohortProspectId
+	 */
+	public function testGetValidProspectCohortByProspectId(){
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("prospectCohort");
+
+		// create a new ProspectCohort and insert to into mySQL
+		$prospectCohort = new ProspectCohort(null, $this->prospect->getProspectId(), $this->cohort->getCohortId());
+		$prospectCohort->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProspectCohort = ProspectCohort::getProspectCohortByProspectId($this->getPDO(), $prospectCohort->getProspectCohortProspectId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("prospectCohort"));
+		$this->assertEquals($pdoProspectCohort->getProspectCohortProspectId(), $this->prospect->getProspectId());
+		$this->assertEquals($pdoProspectCohort->getProspectCohortCohortId(), $this->cohort->getCohortId());
+	}
+
+	/**
+	 *  test grabbing an ProspectCohort by ProspectCohortProspectId
+	 */
+	public function testGetInvalidProspectCohortByProspectId(){
+		// grab a prospectCohort by searching for id that does not exist
+		$prospectCohort = ProspectCohort::getProspectCohortByProspectId($this->getPDO(), AaaaTest::INVALID_KEY);
+		$this->assertNull($prospectCohort);
+	}
+
+	/**
+	 *  test grabbing all ProspectCohorts
+	 */
+	public function testGetAllValidProspectCohorts(){
+// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("prospectCohort");
+
+		// create a new ProspectCohort and insert to into mySQL
+		$prospectCohort = new ProspectCohort(null, $this->prospect->getProspectId(), $this->cohort->getCohortId());
+		$prospectCohort->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = ProspectCohort::getAllProspectCohorts($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("prospectCohort"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\DdcAaaa\\ProspectCohort", $results);
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProspectCohort = $results[0];
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("prospectCohort"));
+		$this->assertEquals($pdoProspectCohort->getProspectCohortProspectId(), $this->prospect->getProspectId());
+		$this->assertEquals($pdoProspectCohort->getProspectCohortCohortId(), $this->cohort->getCohortId());
+	}
+
 }
