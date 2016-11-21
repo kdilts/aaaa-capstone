12bcdc2afe0f1f0edf46a9e6ab866d480260f42b
Trevor Pelton <tnpflyer20@msn.com>
2016-11-21 15:24:32 -0700

    trying change on line 150

--- public_html/php/test/ApplicationTest.php
+++ public_html/php/test/ApplicationTest.php
@@ -147,7 +147,7 @@ class ApplicationTest extends AaaaTest {
 
 		// grab the result from the array and validate it
 		$pdoApplication = $results[0];
-		$this->assertEquals($pdoApplication->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoApplication->getApplicationId(), $application->getApplicationId());
 		$this->assertEquals($pdoApplication->getApplicationFirstName(), $this->VALID_APPLICATIONFIRSTNAME);
 		$this->assertEquals($pdoApplication->getApplicationDateTime(), $this->VALID_APPLICATIONLASTNAME);
 	}
