d14f700886f19c7b2dd9a30120cd6601992e80e3
Trevor Pelton <tnpflyer20@msn.com>
2016-11-28 16:22:34 -0700

    I hope this works

---
+++ api/formstacksubmittest.php
@@ -0,0 +1,7 @@
+<?php
+// The global $_POST variable allows you to access the data sent with the POST method
+// To access the data sent with the GET method, you can use $_GET
+//$say = htmlspecialchars($_POST['say']);
+//$to  = htmlspecialchars($_POST['to']);
+
+echo  $_POST;
