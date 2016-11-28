f4342e1cc54e2c1a8097b5f3101af787d156c328
Trevor Pelton <tnpflyer20@msn.com>
2016-11-28 10:07:34 -0700

    updated custom tag in index.php for angular 2 spin up

---
+++ webpack/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>AAAAA Capstone</title>
+	</head>
+	<body>
+		<!-- This custom tag much match your Angular @Component selector name in app/app.component.ts -->
+		<aaaa-capstone>Loading&hellip;</aaaa-capstone>
+	</body>
+</html>
