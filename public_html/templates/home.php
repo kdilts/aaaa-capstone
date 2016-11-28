a24e95b4ca469f3c57e931a2a5ff81110dfe0139
Trevor Pelton <tnpflyer20@msn.com>
2016-11-28 10:17:46 -0700

    finishing deployment barebones

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to the Angular 2 Demo!</h1>
+			<p class="lead">This is a bare-bones Angular 2 Front End :D</p>
+		</div>
+	</div>
+</section>
