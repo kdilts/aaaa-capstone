44a8906fa04acf6b96c42d96111cb2476432d8f4
Trevor Pelton <tnpflyer20@msn.com>
2016-11-29 13:53:24 -0700

    writes incoming json to text file

--- api/formstacksubmittest.php
+++ api/formstacksubmittest.php
@@ -8,6 +8,8 @@ $requestContent = file_get_contents("php://input");
 // Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
 
 
-$requestObject = json_decode($requestContent);
+//$requestObject = json_decode($requestContent);
 
-var_dump($requestObject);
+$fd = fopen("/tmp/ASTM-D4236.txt", "w");
+fwrite($fd, $requestContent);
+fclose($fd);
