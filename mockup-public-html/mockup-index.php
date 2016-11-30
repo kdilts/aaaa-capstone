d2977d10527f2e6e6ef1a1ef93f79ce3735e0cfc
jeremiahwood <jwood47@cnm.edu>
2016-11-30 10:11:25 -0700

    mockUp for the Login front End https://app.asana.com/0/202206849274532/219751694958506

--- mockup-public-html/mockup-index.php
+++ mockup-public-html/mockup-index.php
@@ -1,7 +1,60 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jeremiahwood
- * Date: 11/30/16
- * Time: 9:00 AM
- */
+<!DOCTYPE html>
+
+<html>
+	<header lang="en">
+		<meta charset="utf-8"/>
+
+
+	<title>Stream Line CNM Log in Page</title>
+	</header>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<div class="row">
+			<header>
+				<nav class="navbar navbar-inverse">
+					<div class="container">
+						<ul class="nav navbar-nav navbar-right">
+						</ul>
+					</div><!-- /.container -->
+				</nav>
+			</header>
+			</div>
+
+			<main class="container">
+				<h1 class="text-center">Stream Line CNM</h1>
+				<div class="form-wrap row">
+					<div class="col-sm-12 col-sm-offset-12">
+
+						<!-- insert contact form here -->
+						<form id="contact-form">
+							<div class="form-group">
+								<label for="name">Name</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-user"></i>
+									</div>
+									<input class="form-control" type="text" name="name" id="name" placeholder="Your Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email">Email</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-envelope"></i>
+									</div>
+									<input class="form-control" type="email" name="email" id="email" placeholder="Your Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="phone">Phone</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-user"></i>
+									</div>
+									<input class="form-control" type="number" name="number" placeholder="Your Phone">
+								</div>
+							</div>
+		</div>
+	</body>
+</html>
