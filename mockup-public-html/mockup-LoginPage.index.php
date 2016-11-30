a40b01dfeeb2c366ea9f4d08a28b8f88aa24b7fb
jeremiahwood <jwood47@cnm.edu>
2016-11-30 16:46:17 -0700

    mock up for The log in Page https://app.asana.com/0/202206849274532/219751694958506

--- mockup-public-html/mockup-LoginPage.index.php
+++ mockup-public-html/mockup-LoginPage.index.php
@@ -4,7 +4,11 @@
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
+			<script data-require="jquery@2.2.0" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+			<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+			<script data-require="bootstrap@3.3.6" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+			<link rel="stylesheet" href="style.css" />
+			<script src="script.js"></script>
 		<title>Stream Line CNM Log in Page</title>
 	</head>
 
@@ -23,12 +27,11 @@
 			<main>
 				<div class="container">
 					<div class="jumbotron text-center">
+						<h1 class="text-center">Stream Line CNM</h1>
+						<div class="form-wrap row">
+							<div class="col-sm-12 col-sm-offset-12">
 					</div>
 				</div>
-				<h1 class="text-center">Stream Line CNM</h1>
-				<div class="form-wrap row">
-					<div class="col-sm-12 col-sm-offset-12">
-
 						<!-- insert contact form here -->
 						<form id="contact-form">
 							<div class="form-group">
