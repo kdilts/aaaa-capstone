d067cc589dde49827c3d001c83e10eb83f0cd48b
jeremiahwood <jwood47@cnm.edu>
2016-12-01 11:44:33 -0700

    mock up log in page, adjusted the position of log in bars https://app.asana.com/0/202206849274532/219751694958506

--- mockup-public-html/mockup-LoginPage.index.php
+++ mockup-public-html/mockup-LoginPage.index.php
@@ -4,11 +4,14 @@
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-			<script data-require="jquery@2.2.0" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-			<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-			<script data-require="bootstrap@3.3.6" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-			<link rel="stylesheet" href="style.css" />
-			<script src="script.js"></script>
+		<script data-require="jquery@2.2.0" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="script.js"></script>
 		<title>Stream Line CNM Log in Page</title>
 	</head>
 
@@ -16,23 +19,26 @@
 		<div class="sfooter-content">
 			<div class="row"></div>
 			<header>
-				<nav class="navbar navbar-inverse">
-					<div class="container">
-						<ul class="nav navbar-nav navbar-right">
-						</ul>
-					</div>
-					<!-- container -->
-				</nav>
+				<div class="container">
+				</div>
 			</header>
-
+			<!-- container -->
 			<main>
 				<div class="container">
 					<div class="jumbotron text-center">
 						<h1 class="text-center">Stream Line CNM</h1>
 						<div class="form-wrap row">
 							<div class="col-sm-12 col-sm-offset-12">
+							</div>
+						</div>
+						</div>
 					</div>
-				</div>
+						<div class="container">
+							<div class="row">
+								<div class="col-sm-12">
+								</div>
+							</div>
+						</div>
 						<!-- insert contact form here -->
 						<form id="contact-form">
 							<div class="form-group">
@@ -41,7 +47,7 @@
 									<div class="input-group-addon">
 										<i class="fa fa-user"></i>
 									</div>
-									<input class="form-control" type="text" name="email" id="email" placeholder="Your Email">
+									<input class="form-control" type="text" name="email" value size="40" id="email" placeholder="Your Email">
 								</div>
 							</div>
 							<div class="form-group">
@@ -50,9 +56,10 @@
 									<div class="input-group-addon">
 										<i class="fa fa-envelope"></i>
 									</div>
-									<input class="form-control" type="text" name="password" id="password" placeholder="Password">
+									<input class="form-control" type="text" name="password" value size="20" id="password" placeholder="Password">
 								</div>
 							</div>
-		</div>
+					</div>
+					</main>
 	</body>
 </html>
