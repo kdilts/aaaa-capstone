ff29d5b65a37acebb11900a22697d90da03eb5f7
jeremiahwood <jwood47@cnm.edu>
2016-12-02 15:07:22 -0700

    mock up  page for Applicant,  added information for new student
    https://app.asana.com/0/202206849274532/223934343881578

--- mockup-public-html/mockup-applicant.php
+++ mockup-public-html/mockup-applicant.php
@@ -76,7 +76,41 @@
 						<h4 class="modal-title" id="myModalLabel">Add Student</h4>
 					</div>
 					<div class="modal-body">
-						...make form here!
+						<label for="name">Name <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-user" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="subject">Subject</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-pencil" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message">Message <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-comment" aria-hidden="true"></i>
+							</div>
+							<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+						</div>
+					</div>
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
