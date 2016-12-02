f2577f6229e2e044372364b2f2e227b660fa8778
jeremiahwood <jwood47@cnm.edu>
2016-12-02 10:54:07 -0700

    mock up FORM page for prospect
    https://app.asana.com/0/202206849274532/223934343881597

---
+++ mockup-public-html/mockup-form/mockup-form-prospect.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<script data-require="jquery@2.2.0" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="script.js"></script>
+
+		<title>Insert Page CNM</title>
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="jumbotron text-center">
+					<h1>Insert Page Stream Line CNM</h1>
+				</div>
+			</div>
+		</section>
+		<div class="list-group">
+			<ul>
+			<button type="button" class="list-group-item">Prospect</button>
+			<button type="button" class="list-group-item">Proscpect Id</button>
+			<button type="button" class="list-group-item">First Name</button>
+			<button type="button" class="list-group-item">Last Name</button>
+			<button type="button" class="list-group-item">Email</button>
+			<button type="button" class="list-group-item">Phone</button>
+			</ul>
+
+		</div>
+	</body>
+</html>
