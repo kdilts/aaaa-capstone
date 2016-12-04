abe472ff31de56507f55bae6b1638c604c13bd2e
jeremiahwood <jwood47@cnm.edu>
2016-12-04 15:26:55 -0700

    mock up  page for prospect,  added student information
    https://app.asana.com/0/202206849274532/223934343881597

--- mockup-public-html/mockup-form/mockup-form-prospect.php
+++ mockup-public-html/mockup-form/mockup-form-prospect.php
@@ -24,19 +24,47 @@
 			</div>
 		</section>
 		<div class="list-group">
-				<div class="row">
-					<div class="col-sm-6">
-					</div>
-			<ul>
-				<div class="list-group">
-					<a href="#" class="list-group-item">Prospect ID</a>
-					<a href="#" class="list-group-item">Name</a>
-					<a href="#" class="list-group-item">Email</a>
-					<a href="#" class="list-group-item">Phone</a>
+			<div>
+				<div class="col-sm-6">
+					<form class="form-horizontal">
+						<div class="form-group">
+							<div class="form-group">
+								<label for="inputprospectid3" class="col-sm-2 control-label">Prospect Id</label>
+								<div class="col-sm-10">
+									<input type="prospect" class="form-control" id="inputProspectId33" placeholder="Prospect Id">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputName3" class="col-sm-2 control-label">Name</label>
+								<div class="col-sm-10">
+									<input type="name" class="form-control" id="inputName3" placeholder="Name">
+								</div>
+							</div>
+							<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-10">
+								<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputPhone3" class="col-sm-2 control-label">Phone #</label>
+							<div class="col-sm-10">
+								<input type="phone" class="form-control" id="inputPhone33" placeholder="Phone Number">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<button type="submit" class="btn btn-default">Sign in</button>
+							</div>
+						</div>
+					</form>
 				</div>
+
 				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
-				</div>
-			</ul>
+			</div>
 		</div>
 	</body>
 </html>
