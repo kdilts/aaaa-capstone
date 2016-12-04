c9f6140ec924ade3961ee906aa910e580b99c7d0
jeremiahwood <jwood47@cnm.edu>
2016-12-04 16:56:51 -0700

    mock up  page for prospect,Cohort  added student information
    https://app.asana.com/0/202206849274532/223934343881598

---
+++ mockup-public-html/mockup-prospectcohort.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<script data-require="jquery@2.2.0" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="script.js"></script>
+
+		<title>Prospect Cohort CNM</title>
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="jumbotron text-center">
+					<h1>Insert Page Prospect Cohort Stream Line CNM</h1>
+				</div>
+			</div>
+		</section>
+		<div class="list-group">
+			<div>
+				<div class="col-sm-6">
+					<form class="form-horizontal">
+						<div class="form-group">
+							<div class="form-group">
+								<label for="inputprospectid3" class="col-sm-2 control-label">Prospect Cohort Id</label>
+								<div class="col-sm-10">
+									<input type="prospect" class="form-control" id="inputProspectId33" placeholder="Prospect Id">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputName3" class="col-sm-2 control-label">Name</label>
+								<div class="col-sm-10">
+									<input type="name" class="form-control" id="inputName3" placeholder="Name">
+								</div>
+							</div>
+							<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-10">
+								<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputPhone3" class="col-sm-2 control-label">Phone #</label>
+							<div class="col-sm-10">
+								<input type="phone" class="form-control" id="inputPhone33" placeholder="Phone Number">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<button type="submit" class="btn btn-default">Sign in</button>
+							</div>
+						</div>
+					</form>
+				</div>
+
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
+			</div>
+		</div>
+	</body>
+</html>
