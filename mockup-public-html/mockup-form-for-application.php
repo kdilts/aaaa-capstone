9a424178f660b086c22ea232d38cc890a38ed5a6
Maria Rosado <mrosado2@cnm.edu>
2016-12-05 11:39:22 -0700

    created form for note mockup file                                                                                                                                                       https://app.asana.com/0/202206849274532/223934343881594

--- mockup-public-html/mockup-form-for-application.php
+++ mockup-public-html/mockup-form-for-application.php
@@ -43,7 +43,7 @@
 		</form>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
 
-		<script; src="bootstrap/js/bootstrap.min.js"></script>
+			<script; src="bootstrap/js/bootstrap.min.js"></script>
 	</body>
 
-	</html>
+</html>
