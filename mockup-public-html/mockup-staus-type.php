a3e70a777a6bab682f912c956e06ee83c9af0e2d
jeremiahwood <jwood47@cnm.edu>
2016-12-04 17:11:52 -0700

    mock up  page for Statys Tyoe
    https://app.asana.com/0/202206849274532/223934343881599

---
+++ mockup-public-html/mockup-staus-type.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<script data-require="jquery@2.2.0" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="script.js"></script>
+
+		<title>Insert Page CNM</title>
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="jumbotron text-center">
+					<h1>Insert Page Stream Line CNM</h1>
+				</div>
+			</div>
+		</section>
+		<div class="list-group">
+			<div>
+				<div class="col-sm-6">
+					<form class="form-horizontal">
+						<div class="form-group">
+							<div class="form-group">
+								<label for="inputstatustype3" class="col-sm-2 control-label">Status Type Id</label>
+								<div class="col-sm-10">
+									<input type="statusttype" class="form-control" id="inputstatustype3" placeholder="Status Type Id">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputstatustypeName3" class="col-sm-2 control-label">Status Type Name</label>
+								<div class="col-sm-10">
+									<input type="statustypename" class="form-control" id="inputstatustypename3" placeholder="StatusTyoeName">
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<button type="enter" class="btn btn-default">Enter</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
