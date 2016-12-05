2a0815817d31a533da175c6687381715f8044176
jeremiahwood <jwood47@cnm.edu>
2016-12-05 10:06:28 -0700

    mock up  page for Student Permit
    https://app.asana.com/0/202206849274532/223934343881600

---
+++ mockup-public-html/mockup-student-permit.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<script data-require="jquery@2.2.0" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="script.js"></script>
+
+		<title>Insert Page CNM</title>
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="jumbotron text-center">
+					<h1>Student Permit Insert Page Stream Line CNM</h1>
+				</div>
+			</div>
+		</section>
+		<div class="list-group">
+			<div>
+				<div class="col-sm-6">
+					<form class="form-horizontal">
+						<div class="form-group">
+							<div class="form-group">
+								<label for="studentpermitid3" class="col-sm-2 control-label">Student Permit Id</label>
+								<div class="col-sm-10">
+									<input type="studentpermitid" class="form-control" id="inputStudentPermitId3" placeholder="StudentPermitId">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputStudentpermitApplicationId3" class="col-sm-2 control-label">Name</label>
+								<div class="col-sm-10">
+									<input type="studentpermitapplicaitonid" class="form-control" id="inputstudentpermitapplicationid3" placeholder="Student Permit Application Id">
+								</div>
+							</div>
+							<label for="inputStudentPermitSwipeId3" class="col-sm-2 control-label">Student Permit Swipe Id</label>
+							<div class="col-sm-10">
+								<input type="studentpermitswipeid" class="form-control" id="inputstudentpermitswipeid3" placeholder="Student Permit Swipe Id">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputStudentPermitPlacardId3" class="col-sm-2 control-label">Student Permit Placard Id #</label>
+							<div class="col-sm-10">
+								<input type="StudentPermitPlacardId" class="form-control" id="inputStudentPermitPlacardId3" placeholder="Student Permit Placard Id">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								<button type="submit" class="btn btn-default">Submit</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
