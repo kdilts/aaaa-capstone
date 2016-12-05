12a12da7210d0e1ee5b073715407284a60451514
jeremiahwood <jwood47@cnm.edu>
2016-12-05 10:26:26 -0700

    page for Swipe mock up
    https://app.asana.com/0/202206849274532/223934343881601

---
+++ mockup-public-html/mockup-swipe.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<script data-require="jquery@2.2.0" data-semver="2.2.0"
+				  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="style.css"/>
+		<script src="script.js"></script>
+
+		<title>Insert Page CNM</title>
+	</head>
+	<body>
+			<section>
+				<div class="container">
+					<div class="jumbotron text-center">
+						<h1>Swipe Insert Page Stream Line CNM</h1>
+					</div>
+				</div>
+			</section>
+			<div class="list-group">
+				<div>
+					<div class="col-sm-6">
+						<form class="form-horizontal">
+							<div class="form-group">
+								<div class="form-group">
+									<label for="swipeId3" class="col-sm-2 control-label">Swipe Id</label>
+									<div class="col-sm-10">
+										<input type="swipeid" class="form-control" id="inputSwipeId3" placeholder="SwipeId">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="inputSwipeNumber3" class="col-sm-2 control-label">Swipe Number</label>
+									<div class="col-sm-10">
+										<input type="swipenumber" class="form-control" id="inputSwipeNumber3" placeholder="Swipe Number">
+									</div>
+								</div>
+								<label for="inputSwipeStatusTypeId3" class="col-sm-2 control-label">Swipe Status Type Id</label>
+								<div class="col-sm-10">
+									<input type="swipestatustypeId" class="form-control" id="inputswipestatustypeid3" placeholder="Swipe Status Type Id">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-offset-2 col-sm-10">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-offset-2 col-sm-10">
+									<button type="submit" class="btn btn-default">Submit</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+	</body>
+
+
+
+	</html>
