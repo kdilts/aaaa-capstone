fc9ddad22239622f08fe1813a0673b7b31fefc6b
Kevin Dilts <kdilts13@gmail.com>
2016-12-07 15:28:25 -0700

    working on creating angular form

--- app/components/noteType-component.ts
+++ app/components/noteType-component.ts
@@ -1,14 +1,18 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {Router} from "@angular/router";
 import {NoteType} from "../classes/noteType";
 import {NoteTypeService} from "../services/noteType-service";
+import {Status} from "../classes/status";
 
 @Component({
 	templateUrl: "./templates/noteType.php"
 })
 
 export class NoteTypeComponent implements OnInit {
-	private noteTypes : NoteType[] = [];
+	@ViewChild("noteTypeForm") noteTypeForm;
+	noteTypes : NoteType[] = [];
+	NoteType : NoteType = new NoteType(null, "");
+	status: Status = null;
 
 	constructor(private noteTypeService: NoteTypeService, private router: Router) {}
 
@@ -21,5 +25,20 @@ export class NoteTypeComponent implements OnInit {
 			.subscribe(noteTypes => this.noteTypes = noteTypes);
 	}
 
+	reloadNoteTypes() : void {
+		this.noteTypeService.getAllNoteType()
+			.subscribe(noteTypes => this.noteTypes = noteTypes);
+	}
+
+	createNoteType() : void {
+		this.noteTypeService.createNoteType(this.NoteType)
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+					this.reloadNoteTypes();
+					this.noteTypeForm.reset();
+				}
+			});
+	}
 
 }
