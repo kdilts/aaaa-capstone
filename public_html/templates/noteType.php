f265d7cc90c197991209648102d869565fe9019a
Kevin Dilts <kdilts13@gmail.com>
2016-12-07 16:01:49 -0700

    renamed fields for consistency

--- public_html/templates/noteType.php
+++ public_html/templates/noteType.php
@@ -9,17 +9,17 @@
 <form #noteTypeForm="ngForm" name="noteTypeForm" id="noteTypeForm" class="form-horizontal well" (ngSubmit)="createNoteType();" novalidate>
 	<h2>Create NoteType</h2>
 	<hr />
-	<div class="form-group" [ngClass]="{ 'has-error': noteType.touched && noteType.invalid }">
+	<div class="form-group" [ngClass]="{ 'has-error': noteTypeName.touched && noteTypeName.invalid }">
 		<label for="noteType">NoteType</label>
 		<div class="input-group">
 			<div class="input-group-addon">
 				<i class="fa fa-comment" aria-hidden="true"></i>
 			</div>
-			<input type="text" name="noteType" id="noteType" class="form-control" maxlength="255" required [(ngModel)]="noteType.noteType" #noteTypeText="ngModel" />
+			<input type="text" name="noteTypeName" id="noteTypeName" class="form-control" maxlength="255" required [(ngModel)]="noteType.noteTypeName" #noteTypeName="ngModel" />
 		</div>
-		<div [hidden]="noteTypeText.valid || noteTypeText.pristine" class="alert alert-danger" role="alert">
-			<p *ngIf="noteTypeText.errors?.required">NoteType is required.</p>
-			<p *ngIf="noteTypeText.errors?.maxlength">NoteType is too long. You typed</p>
+		<div [hidden]="noteTypeName.valid || noteTypeName.pristine" class="alert alert-danger" role="alert">
+			<p *ngIf="noteTypeName.errors?.required">NoteType is required.</p>
+			<p *ngIf="noteTypeName.errors?.maxlength">NoteType is too long. You typed</p>
 		</div>
 	</div>
 	<button type="submit" class="btn btn-info btn-lg" [disabled]="noteTypeForm.invalid"><i class="fa fa-share"></i> NoteType</button>
