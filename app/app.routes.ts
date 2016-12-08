8c35692158f92de1284cbb6c6cff10019536f7f5
Kevin Dilts <kdilts13@gmail.com>
2016-12-08 10:29:59 -0700

    added routes

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,12 +1,18 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {NoteTypeComponent} from "./components/noteType-component";
+import {DetailViewComponent} from "./components/detailView-component";
+import {PkgViewComponent} from "./components/pkgView-component";
+import {AppViewComponent} from "./components/appView-component";
 
-export const allAppComponents = [HomeComponent, NoteTypeComponent];
+export const allAppComponents = [HomeComponent, NoteTypeComponent, AppViewComponent, PkgViewComponent, DetailViewComponent ];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
-	{path: "noteType", component: NoteTypeComponent }
+	{path: "noteType", component: NoteTypeComponent },
+	{path: "appView", component: AppViewComponent },
+	{path: "pkgView", component: PkgViewComponent },
+	{path: "detailView", component: DetailViewComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
