90b408b6fea7d4dcb48d029dda1c8d54324a717a
jeremiahwood <jwood47@cnm.edu>
2016-12-08 12:35:04 -0700

    Page for the APP view, changed to nav bar and table with 2 tabs
    https://app.asana.com/0/202206849274532/227585475317776

--- mockup-public-html/mockup-applicant.php
+++ mockup-public-html/mockup-applicant.php
@@ -24,7 +24,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Brand</a>
+					<a class="navbar-brand" href="#">CNM</a>
 				</div>
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
