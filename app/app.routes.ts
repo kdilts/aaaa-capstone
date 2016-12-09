bcda5acb4d8cf3e28eae835c50566fe92369f535
Kevin Dilts <kdilts13@gmail.com>
2016-12-09 15:22:43 -0700

    adding prsview component

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -4,13 +4,22 @@ import {NoteTypeComponent} from "./components/noteType-component";
 import {DetailViewComponent} from "./components/detailView-component";
 import {PkgViewComponent} from "./components/pkgView-component";
 import {AppViewComponent} from "./components/appView-component";
+import {PrsViewComponent} from "./components/prsView-component";
 
-export const allAppComponents = [HomeComponent, NoteTypeComponent, AppViewComponent, PkgViewComponent, DetailViewComponent ];
+export const allAppComponents = [
+	HomeComponent,
+	NoteTypeComponent,
+	AppViewComponent,
+	PkgViewComponent,
+	PrsViewComponent,
+	DetailViewComponent
+];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "noteType", component: NoteTypeComponent },
 	{path: "appView", component: AppViewComponent },
+	{path: "prsView", component: AppViewComponent },
 	{path: "pkgView", component: PkgViewComponent },
 	{path: "detailView/:applicationId", component: DetailViewComponent }
 ];
---
+++ app/components/prsView-component.ts
@@ -0,0 +1,63 @@
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {Router, ActivatedRoute} from "@angular/router";
+import {Application} from "../classes/application";
+import {ApplicationService} from "../services/application-service";
+import {Prospect} from "../classes/prospect";
+import {ProspectService} from "../services/prospect-service";
+import {Cohort} from "../classes/cohort";
+import {CohortService} from "../services/cohort-service";
+import {ApplicationCohort} from "../classes/applicationCohort";
+import {ApplicationCohortService} from "../services/applicationCohort-service";
+import {Status} from "../classes/status";
+
+@Component({
+	templateUrl: "./templates/prsView.php"
+})
+
+export class PrsViewComponent implements OnInit{
+	@ViewChild("prsView") prsView : any;
+	applications : Application[] = [];
+	prospects : Prospect[] = [];
+	applicationCohorts : ApplicationCohort[] = [];
+	cohorts : Cohort[] = [];
+	status: Status = null;
+
+	constructor(
+		private applicationService: ApplicationService,
+		private prospectService: ProspectService,
+		private applicationCohortService: ApplicationCohortService,
+		private cohortService: CohortService,
+		private router: Router,
+		private route: ActivatedRoute
+	) {}
+
+	ngOnInit() : void {
+		this.reloadApplications();
+		this.reloadProspects();
+		this.reloadApplicationCohorts();
+		this.reloadCohorts();
+	}
+
+	reloadApplications()	 : void {
+		this.applicationService.getAllApplications()
+			.subscribe(applications => this.applications = applications);
+	}
+
+	reloadProspects() : void {
+		this.prospectService.getAllProspects()
+			.subscribe(prospects => this.prospects = prospects);
+	}
+
+	reloadApplicationCohorts()	 : void {
+		this.applicationCohortService.getAllApplicationCohorts()
+			.subscribe(applicationCohorts => this.applicationCohorts = applicationCohorts);
+	}
+
+	reloadCohorts() : void {
+		this.cohortService.getAllCohorts()
+			.subscribe(cohorts => this.cohorts = cohorts);
+	}
+	switchApplication(application: Application) : void {
+		this.router.navigate(["/detailView/", application.applicationId]);
+	}
+}
