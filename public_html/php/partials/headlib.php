76ef1589436b8575c473fc0e303395bc6b3c1471
Kevin Dilts <kdilts13@gmail.com>
2016-12-09 12:25:01 -0700

    added php partial files - navbar and header bootstrap imports

---
+++ public_html/php/partials/headlib.php
@@ -0,0 +1,6 @@
+<script data-require="jquery@2.2.0" data-semver="2.2.0"
+		  src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<link data-require="bootstrap@3.3.6" data-semver="3.3.6" rel="stylesheet"
+		href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+<script data-require="bootstrap@3.3.6" data-semver="3.3.6"
+		  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
---
+++ public_html/php/partials/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-default">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+				  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#">CNM</a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<form class="navbar-form navbar-right">
+			<div class="form-group">
+				<input type="text" class="form-control" placeholder="Search">
+			</div>
+			<button type="submit" class="btn btn-default">Search</button>
+		</form>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Sign Out</a></li>
+			<li><a href="#">Parking</a></li>
+			<li><a href="#">Student</a></li>
+
+			<li role="separator" class="divider"></li>
+		</ul>
+	</div><!-- /.navbar-collapse -->
+</nav>
