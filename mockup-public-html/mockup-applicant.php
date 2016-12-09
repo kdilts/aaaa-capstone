12ad8e4fa2588df4ad4a41f70c672f43a416cb12
jeremiahwood <jwood47@cnm.edu>
2016-12-09 11:37:51 -0700

    added nav bar to application mockup, and pagination table with number tabs at the bottom. almost done just gotta link up the links
    https://app.asana.com/0/202206849274532/227585475317776

--- mockup-public-html/mockup-applicant.php
+++ mockup-public-html/mockup-applicant.php
@@ -214,50 +214,6 @@
 				</li>
 			</ul>
 		</div>
-
-
-					<div class="modal-body">
-						<label for="name">Name <span class="text-danger">*</span></label>
-						<div class="input-group">
-							<div class="input-group-addon">
-								<i class="fa fa-user" aria-hidden="true"></i>
-							</div>
-							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="email">Email <span class="text-danger">*</span></label>
-						<div class="input-group">
-							<div class="input-group-addon">
-								<i class="fa fa-envelope" aria-hidden="true"></i>
-							</div>
-							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="subject">Subject</label>
-						<div class="input-group">
-							<div class="input-group-addon">
-								<i class="fa fa-pencil" aria-hidden="true"></i>
-							</div>
-							<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="message">Message <span class="text-danger">*</span></label>
-						<div class="input-group">
-							<div class="input-group-addon">
-								<i class="fa fa-comment" aria-hidden="true"></i>
-							</div>
-							<textarea class="form-control" rows="5" id="message" name="message"
-										 placeholder="Message (2000 characters max)"></textarea>
-						</div>
-					</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
-				</div>
-
 	</body>
 
 </html>
