e139796314db3584fffad11e635a4749ffc36b2c
jeremiahwood <jwood47@cnm.edu>
2016-12-09 14:55:17 -0700

    Got the nav link and header links in partials file properly routed. for application page https://app.asana.com/0/202206849274532/227585475317776

--- mockup-public-html/mockup-applicant.php
+++ mockup-public-html/mockup-applicant.php
@@ -10,40 +10,13 @@
 				  src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="style.css"/>
 		<script src="script.js"></script>
-		<title> Streamline CNM Applicant</title>
+
+		<?php require_once("../public_html/php/partials/headlib.php"); ?>
+
+		<?php require_once("../public_html/php/partials/navbar.php"); ?>		<title> Streamline CNM Applicant</title>
+
 	</head>
 	<body>
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-							  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">CNM</a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<form class="navbar-form navbar-right">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">Search</button>
-					</form>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Student</a></li>
-						<li><a href="#">Parking</a></li>
-						<li><a href="#">Sign Out</a></li>
-						<li role="separator" class="divider"></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</div><!-- /.container-fluid -->
-		</nav>
 		<section>
 			<div class="container">
 				<ul class="nav nav-tabs nav-justified">
@@ -75,7 +48,7 @@
 					<th>Position</th>
 					<th>Office</th>
 					<th>Age</th>
-					<th>Star</th>
+					<th>Date</th>
 					<th></th>
 				</tr>
 			</tfoot>
