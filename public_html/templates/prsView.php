04c65749709cb812882bc145029ac0931c6d15a4
Kevin Dilts <kdilts13@gmail.com>
2016-12-09 16:57:08 -0700

    routerlink

--- public_html/templates/prsView.php
+++ public_html/templates/prsView.php
@@ -1,49 +1,35 @@
-<!DOCTYPE html>
-<html>
-	<head lang="en">
-		<?php require_once("../php/partials/headlib.php"); ?>
-		<title>Streamline CNM</title>
-	</head>
-
-	<body>
-		<div class="container">
-			<?php require_once("../php/partials/navbar.php"); ?>
-
-			<section>
-				<div class="row">
-					<div class="col-sm-12">
-						<ul class="nav nav-tabs nav-justified">
-							<li role="presentation"><a href="appView.php">Applicants</a></li>
-							<li role="presentation"><a href="prsView.php">Prospect</a></li>
-						</ul>
-					</div>
-				</div>
-			</section>
-
-			<section>
-				<div class="row">
-					<div class="col-xs-12">
-						<table class="table table-bordered table-hover">
-							<thead>
-								<tr>
-									<th>Last</th>
-									<th>First</th>
-									<th>Email</th>
-									<th>Cohort</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr *ngFor="let prospectCohort of prospectCohorts">
-									<td>{{ prospectCohort.info[0].prospectFirstName }}</td>
-									<td>{{ prospectCohort.info[0].prospectLastName }}</td>
-									<td>{{ prospectCohort.info[0].prospectEmail }}</td>
-									<td>{{ prospectCohort.info[1].cohortName }}</td>
-								</tr>
-							</tbody>
-						</table>
-					</div><!--end of .table-responsive-->
-				</div>
-			</section>
+<section>
+	<div class="row">
+		<div class="col-sm-12">
+			<ul class="nav nav-tabs nav-justified">
+				<li role="presentation"><a routerLink="appView">Applicants</a></li>
+				<li role="presentation"><a routerLink="prsView">Prospect</a></li>
+			</ul>
 		</div>
-	</body>
-</html>
+	</div>
+</section>
+
+<section>
+	<div class="row">
+		<div class="col-xs-12">
+			<table class="table table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>Last</th>
+						<th>First</th>
+						<th>Email</th>
+						<th>Cohort</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr *ngFor="let prospectCohort of prospectCohorts">
+						<td>{{ prospectCohort.info[0].prospectFirstName }}</td>
+						<td>{{ prospectCohort.info[0].prospectLastName }}</td>
+						<td>{{ prospectCohort.info[0].prospectEmail }}</td>
+						<td>{{ prospectCohort.info[1].cohortName }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div><!--end of .table-responsive-->
+	</div>
+</section>
