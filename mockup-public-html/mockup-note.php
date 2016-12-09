74a039f2e9ab1345882625879acd3a422c2b84fe
Maria Rosado <mrosado2@cnm.edu>
2016-12-09 16:40:16 -0700

    changed note date picker

--- mockup-public-html/mockup-note.php
+++ mockup-public-html/mockup-note.php
@@ -34,10 +34,9 @@
 		<div class="row">
 			<div class='col-sm-6'>
 				<div class="form-group">
-					<div class='input-group date' id='datetimepicker1'>
-						<input type='text' class="form-control" />
-						<span class="input-group-addon">
-							<span class="glyphicon glyphicon-calendar"></span>
+					<section>
+						<input type="date">
+						</sect
 						</span>
 					</div>
 				</div>
