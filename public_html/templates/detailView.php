5d675a6af5cba28ace374b8427da7dabd1437960
jeremiahwood <jwood47@cnm.edu>
2016-12-09 15:41:23 -0700

    added nav barhttps://app.asana.com/0/202206849274532/227585475317778

--- public_html/templates/detailView.php
+++ public_html/templates/detailView.php
@@ -1,2 +1,14 @@
-<h1>Detail View</h1>
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<?php require_once("../php/partials/headlib.php"); ?>
+		<title>Streamline CNM</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<?php require_once("../php/partials/navbar.php"); ?>
+
+
+			<h1>Detail View</h1>
 <h3>First Name: {{application.applicationFirstName}}</h3>
