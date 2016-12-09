e17905a190064b08447de4ce2e3e42a6d1b9cbcd
Trevor Pelton <tnpflyer20@msn.com>
2016-12-09 16:11:23 -0700

    added rest of the fields for detailView

--- public_html/templates/detailView.php
+++ public_html/templates/detailView.php
@@ -11,6 +11,14 @@
 
 			<h1>Detail View</h1>
 			<h3>First Name: {{application.applicationFirstName}}</h3>
+			<h3>Last Name: {{application.applicationLastName}}</h3>
+			<h3>Email: {{application.applicationEmail}}</h3>
+			<h3>Phone Number: {{application.applicationPhoneNumber}}</h3>
+			<h3>Where did you hear about DDC from? {{application.applicationSource}}</h3>
+			<h3>About You:{{application.applicationAboutYou}}</h3>
+			<h3>Hope To Accomplish: {{application.applicationHopeToAccomplish}}</h3>
+			<h3>Previous Experience: {{application.applicationExperience}}</h3>
+			<h3>Date Submitted: {{application.applicationDateTime}}</h3>
 		</div>
 	</body>
 </html>
