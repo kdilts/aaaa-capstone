600dad78a618c1d3a1df51b18b3cf2a4abfdb1d9
Trevor Pelton <tnpflyer20@msn.com>
2016-12-10 14:59:19 -0700

    trying modal

--- public_html/templates/detailView.php
+++ public_html/templates/detailView.php
@@ -51,7 +51,26 @@
 			<thead>
 				<tr>
 					<td>Notes:</td>
-					<td><button>+</button></td>
+					<td><button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">+</button>
+						<div id="myModal" class="modal fade">
+							<div class="modal-dialog">
+								<div class="modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+										<h4 class="modal-title">Confirmation</h4>
+									</div>
+									<div class="modal-body">
+										<p>Do you want to save changes you made to document before closing?</p>
+										<p class="text-warning"><small>If you don't save, your changes will be lost.</small></p>
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+										<button type="button" class="btn btn-primary">Save changes</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</td>
 				</tr>
 				<tr>
 					<td>Date</td>
