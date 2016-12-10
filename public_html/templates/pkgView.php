87b39f900098c29899fcd7a7d7dc1eaf7226562b
Kevin Dilts <kdilts13@gmail.com>
2016-12-10 16:41:07 -0700

    updated parking view w/ placeholder table

--- public_html/templates/pkgView.php
+++ public_html/templates/pkgView.php
@@ -1,1 +1,31 @@
-<h1>parking view</h1>
+<div class="row">
+	<div class="col-xs-12">
+		<table class="table table-bordered table-hover">
+			<thead>
+				<tr>
+					<td>Parking:</td>
+				</tr>
+				<tr>
+					<td>Last</td>
+					<td>First</td>
+					<td>Cohort</td>
+					<td>Swipe #</td>
+					<td>Placard #</td>
+					<td>Check Out Date</td>
+					<td>Check In Date</td>
+					<td>Status</td>
+				</tr>
+			</thead>
+			<tr>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+			</tr>
+		</table>
+	</div>
+</div>
