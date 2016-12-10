e4b4c95156bf6a43f57956641a6def06b0f6c5aa
Trevor Pelton <tnpflyer20@msn.com>
2016-12-10 13:11:06 -0700

    working listed detail view

--- public_html/templates/detailView.php
+++ public_html/templates/detailView.php
@@ -1,10 +1,12 @@
 <h1>Detail View</h1>
-<h3>First Name: {{application.applicationFirstName}}</h3>
-<h3>Last Name: {{application.applicationLastName}}</h3>
-<h3>Email: {{application.applicationEmail}}</h3>
-<h3>Phone Number: {{application.applicationPhoneNumber}}</h3>
-<h3>Where did you hear about DDC from? {{application.applicationSource}}</h3>
-<h3>About You:{{application.applicationAboutYou}}</h3>
-<h3>Hope To Accomplish: {{application.applicationHopeToAccomplish}}</h3>
-<h3>Previous Experience: {{application.applicationExperience}}</h3>
-<h3>Date Submitted: {{application.applicationDateTime}}</h3>
+<ul>
+	<li><em>First Name: </em>{{application.applicationFirstName}}</li>
+	<li><em>Last Name: </em>{{application.applicationLastName}} </li>
+	<li><em>Email: </em>{{application.applicationEmail}}</li>
+	<li><em>Phone Number: </em>{{application.applicationPhoneNumber}}</li>
+	<li><em>Where did you hear about DDC from? </em>{{application.applicationSource}}</li>
+	<li><em>About You: </em>{{application.applicationAboutYou}}</li>
+	<li><em>Hope To Accomplish: </em>{{application.applicationHopeToAccomplish}}</li>
+	<li><em>Previous Experience: </em>{{application.applicationExperience}}</li>
+	<li><em>Date Submitted: </em>{{application.applicationDateTime}}</li>
+</ul>
