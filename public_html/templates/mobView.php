91f54238b25f68fa9cdad4fdf8afa69fc5493271
Trevor Pelton <tnpflyer20@msn.com>
2016-12-13 13:03:51 -0700

    added disabled feature to mobview

--- public_html/templates/mobView.php
+++ public_html/templates/mobView.php
@@ -75,7 +75,7 @@
 
 				<div class="form-group">
 					<div class="col-sm-offset-2 col-sm-10">
-						<button type="submit" class="btn btn-info btn-lg"><i class="fa fa-share"></i> Submit</button>
+						<button type="submit" class="btn btn-info btn-lg" [disabled]="quickProspectForm.invalid"><i class="fa fa-share"></i> Submit</button>
 						<button type="reset" class="btn btn-warning btn-lg"><i class="fa fa-ban"></i> Reset</button>
 					</div>
 				</div>
