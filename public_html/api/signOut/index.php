0d653fea219f0aa7f472d06629b4eadf2d45c328
Kevin Dilts <kdilts13@gmail.com>
2016-12-13 16:21:56 -0700

    added sign out api

---
+++ public_html/api/signOut/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once(dirname(__DIR__,2) . "/php/classes/autoload.php");
+require_once (dirname(__DIR__,2) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\DdcAaaa;
+
+/**
+ * api for signout
+ **/
+//prepare default error message
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddcaaaa.ini");
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET"){
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		$_SESSION = [];
+		$reply->message = "You are now signed out";
+	}
+	else {
+		throw (new InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+// encode and return reply to front end caller
+echo json_encode($reply);
