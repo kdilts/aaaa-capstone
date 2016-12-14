0955cfdf28092a1db0ff0902417056d5d16764a4
Kevin Dilts <kdilts13@gmail.com>
2016-12-14 10:22:39 -0700

    fixed pkgView and css

--- public_html/templates/pkgView.php
+++ public_html/templates/pkgView.php
@@ -1,31 +1,26 @@
-<?php
-//if(session_status() !== PHP_SESSION_ACTIVE) {
-//	session_start();
-//}
-//if(empty($_SESSION["adUser"]) === false) { ?>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-xs-12">
-				<h2>Parking</h2>
-				<table class="table table-bordered table-hover table-parking">
-					<thead>
-						<tr>
-							<td>Parking:</td>
-						</tr>
-						<tr>
-							<td>Last</td>
-							<td>First</td>
-							<td>Cohort</td>
-							<td>Swipe #</td>
-							<td>Placard #</td>
-							<td>Check Out Date</td>
-							<td>Check In Date</td>
-							<td>Status</td>
-						</tr>
-					</thead>
-					<tr *ngFor = "let studentPermit of studentPermits">
-						<td>{{studentPermit.info[3].applicationLastName}}</td>
-						<td>{{studentPermit.info[3].applicationFirstName}}</td>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<h2>Parking</h2>
+			<table class="table table-bordered table-hover table-parking">
+				<thead>
+					<tr>
+						<td>Parking:</td>
+					</tr>
+					<tr>
+						<td>Last</td>
+						<td>First</td>
+						<td>Cohort</td>
+						<td>Swipe #</td>
+						<td>Placard #</td>
+						<td>Check Out Date</td>
+						<td>Check In Date</td>
+						<td>Status</td>
+					</tr>
+				</thead>
+				<tr *ngFor = "let studentPermit of studentPermits">
+					<td>{{studentPermit.info[3].applicationLastName}}</td>
+					<td>{{studentPermit.info[3].applicationFirstName}}</td>
 
 					<td>
 						<ul class="list-unstyled">
@@ -35,16 +30,13 @@
 						</ul>
 					</td>
 
-						<td>{{studentPermit.info[1].swipeNumber}}</td>
-						<td>{{studentPermit.info[0].placardNumber}}</td>
-						<td>{{studentPermit.studentPermitCheckOutDate | date: 'medium'}}</td>
-						<td>{{studentPermit.studentPermitCheckInDate | date: 'medium'}}</td>
-						<td>{{studentPermit.info[2].statusTypeName}}</td>
-					</tr>
-				</table>
-			</div>
+					<td>{{studentPermit.info[1].swipeNumber}}</td>
+					<td>{{studentPermit.info[0].placardNumber}}</td>
+					<td>{{studentPermit.studentPermitCheckOutDate | date: 'medium'}}</td>
+					<td>{{studentPermit.studentPermitCheckInDate | date: 'medium'}}</td>
+					<td>{{studentPermit.info[2].statusTypeName}}</td>
+				</tr>
+			</table>
 		</div>
 	</div>
-<?php //} else {
-//
-//}
+</div>
