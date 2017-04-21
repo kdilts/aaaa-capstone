68bfefe1bfb9b1f25cd55d5a0cc55344eada6535
Maria Rosado <mrosado2@cnm.edu>
2017-04-21 13:56:20 -0600

    php for report view

---
+++ public_html/templates/reportView.php
@@ -0,0 +1,28 @@
+<div class="container">
+	<div class="wrapper">
+		<h1>Reports</h1>
+	</div>
+	<div class="data">
+		<form action="index.php" method="POST">
+			<select name"application cohort">
+			<option>Select</option>
+			<option>Cohort Id</option>
+			<option>Application Id</option>
+			<option>Cohort By Cohort Id</option>
+			</select>
+		</form>
+
+		<h1>ingenuity</h1>
+		<p>This is a test</p>
+		<form>
+			<div class="form-group">
+				<label for="exampleInputUsername">Username</label>
+				<input type="username" class="form-control" id="exampleInputUsername" placeholder="Username">
+				<div class="form-group">
+					<label for="exampleInputPassword">Password</label>
+					<input type="password" class="form-control" id="exampleInputPassword" placeholder="=Password">
+				</div>
+				</div>
+		</form>
+	</div>
+</div>
